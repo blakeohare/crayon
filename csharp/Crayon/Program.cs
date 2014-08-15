05c73c5f64c6c716c48c4d6ac1264ab1657e8b54
blakeohare2 <bohabohu@hotmail.com>
2014-08-15 20:21:07 +0100

    Fix up Program.cs a bit. Check in a demos folder.
    ------------------------------
    Imported from Google Code.
    Original SVN rev#: 6

--- csharp/Crayon/Program.cs
+++ csharp/Crayon/Program.cs
@@ -5,17 +5,57 @@
 		static void Main(string[] args)
 		{
 #if DEBUG
-			string sourceFolder = "C:\\Users\\Blake\\Desktop\\flappy_bird\\source";
-			string outputFolder = "C:\\Users\\Blake\\Desktop\\flappy_bird\\output";
+			string sourceFolder = "C:\\Things\\Projects\\Crayon\\Demos\\FlappySquid\\source";
+			string outputFolder = "C:\\Things\\Projects\\Crayon\\Demos\\FlappySquid\\output";
+			string rawPlatform = "py";
+
 #else
-			if (args.Length != 2) throw new System.Exception("Invalid args");
+			if (args.Length != #)
+			{
+				System.Console.WriteLine("Usage:");
+				System.Console.WriteLine("  C:\\Things> crayon.exe sourcefolder outputfolder platform");
+				System.Console.WriteLine("Platforms:");
+				foreach (string line in PLATFORM_USAGE)
+				{
+					System.Console.WriteLine(line);
+				}
+			}
 			string sourceFolder = args[0];
 			string outputFolder = args[1];
 #endif
 
-			PlatformTarget platform = PlatformTarget.JavaScript_Browser;
+			if (!System.IO.Directory.Exists(sourceFolder))
+			{
+				System.Console.WriteLine("Source folder could not be found.");
+				return;
+			}
+
+			if (!System.IO.Directory.Exists(outputFolder))
+			{
+				System.Console.WriteLine("Output folder could not be found.");
+				return;
+			}
+
+			PlatformTarget platform;
+			switch (rawPlatform.ToLowerInvariant())
+			{
+				case "js": platform = PlatformTarget.JavaScript_Browser; break;
+				case "py": platform = PlatformTarget.Python_PyGame; break;
+				default:
+					System.Console.WriteLine("Platform must be one of the following:");
+					foreach (string line in PLATFORM_USAGE)
+					{
+						System.Console.WriteLine(line);
+					}
+					return;
+			}
 			Packager pkg = new Packager(platform, sourceFolder, outputFolder);
 			pkg.Do();
 		}
+
+		private static readonly string[] PLATFORM_USAGE = new string[] {
+			"  js - HTML/JavaScript",
+			"  py - Python (with PyGame)",
+		};
 	}
 }
