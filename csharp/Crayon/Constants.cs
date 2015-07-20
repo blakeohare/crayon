65818fd3f6c9b22be50a66d5748b4afb13ac18b5
Blake O'Hare <mspaintmaestro@gmail.com>
2015-07-19 18:39:23 -0700

    Refactor %%%CONSTANT_REPLACEMENTS%%% because it was horribly implemented before. Was: O(text length * # replacments) and the number of replacements has steadily grown. Now: O(text length)

--- csharp/Crayon/Constants.cs
+++ csharp/Crayon/Constants.cs
@@ -6,39 +6,74 @@ namespace Crayon
 {
 	internal class Constants
 	{
-		private static readonly Dictionary<string, int> values = new Dictionary<string, int>()
-		{
-			{ "TYPE_NATIVE_OBJECT_SCREEN", 1 },
-			{ "TYPE_NATIVE_OBJECT_IMAGE", 2 },
-		};
+		private static readonly Dictionary<string, string> CONSTANT_REPLACEMENTS;
 
-		public static string DoReplacements(string text, Dictionary<string, string> replacements)
+		static Constants()
 		{
+			Dictionary<string, string> constants = new Dictionary<string, string>()
+			{
+				{ "TYPE_NATIVE_OBJECT_SCREEN", "1" },
+				{ "TYPE_NATIVE_OBJECT_IMAGE", "2" },
+			};
+
 			foreach (Types t in Enum.GetValues(typeof(Types)).Cast<Types>())
 			{
-				text = text.Replace("%%%TYPE_" + t.ToString() + "%%%", ((int)t).ToString());
-			}
-
-			foreach (string key in values.Keys)
-			{
-				text = text.Replace("%%%" + key + "%%%", values[key].ToString());
+				constants["TYPE_" + t.ToString()] = ((int)t).ToString();
 			}
 
 			foreach (IOErrors errorType in Enum.GetValues(typeof(IOErrors)).Cast<IOErrors>())
 			{
-				text = text.Replace("%%%IO_ERROR_" + errorType.ToString() + "%%%", "" + (int)errorType);
+				constants["TYPE_" + errorType.ToString()] = ((int)errorType).ToString();
 			}
 
 			foreach (AsyncMessageType type in Enum.GetValues(typeof(AsyncMessageType)).Cast<AsyncMessageType>())
 			{
-				text = text.Replace("%%%ASYNC_MESSAGE_TYPE_" + type + "%%%", ((int)type).ToString());
+				constants["ASYNC_MESSAGE_TYPE_" + type] = ((int)type).ToString();
 			}
 
-			foreach (string key in replacements.Keys)
+			CONSTANT_REPLACEMENTS = constants;
+		}
+
+		public static string DoReplacements(string text, Dictionary<string, string> replacements)
+		{
+			if (text.Contains("%%%"))
 			{
-				text = text.Replace("%%%" + key + "%%%", replacements[key]);
-			}
+				string[] parts = text.Split(new string[] { "%%%" }, StringSplitOptions.None);
+				bool lastWasReplacement = false;
 
+				List<string> replaced = new List<string>() { parts[0] };
+				int i = 1;
+				for (; i < parts.Length - 1; ++i)
+				{
+					string key = parts[i];
+					if (CONSTANT_REPLACEMENTS.ContainsKey(key))
+					{
+						replaced.Add(CONSTANT_REPLACEMENTS[key]);
+						replaced.Add(parts[++i]);
+						lastWasReplacement = true;
+					}
+					else if (replacements.ContainsKey(key))
+					{
+						replaced.Add(replacements[key]);
+						replaced.Add(parts[++i]);
+						lastWasReplacement = true;
+					}
+					else
+					{
+						replaced.Add("%%%");
+						replaced.Add(key);
+						lastWasReplacement = false;
+					}
+				}
+
+				if (!lastWasReplacement)
+				{
+					replaced.Add("%%%");
+					replaced.Add(parts[parts.Length - 1]);
+				}
+
+				return string.Join("", replaced);
+			}
 			return text;
 		}
 	}
