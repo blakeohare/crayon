45dd8787878435c0e18f368785b33562c279dc36
Blake O'Hare <mspaintmaestro@gmail.com>
2016-07-05 17:14:34 -0700

    Experimenting with an alternate approach to PHP references. This approach wraps every type that should be treated as a reference with a dummy array containing 1 item: the thing it points to. Creating copies of this array is harmless when assigned and passed around, the way the VM code was designed for other languages. This will hopefully lead to fewer unexpected bugs and behavior moving forward, particularly making libraries more worry-free without blowing up the test matrix.

--- Resources/php-server/cryutil.php
+++ Resources/php-server/cryutil.php
@@ -1,48 +1,43 @@
 ﻿<?php
-  $nullhack = null;
-  
-  function &array_hack($array) { return $array; }
   
   function pth_noop() { }
   
-	function &pth_new_array($size) {
+	function pth_new_array($size) {
     $output = array();
     while ($size-- > 0) array_push($output, null);
-    return $output;
+    return array($output);
   }
   
-  function &pth_dictionary_get_keys(&$dictionary) {
+  function pth_dictionary_get_keys($dictionary) {
     $output = array();
-    foreach ($dictionary as $key => $ignored) {
+    foreach ($dictionary[0] as $key => $ignored) {
       array_push($output, $key);
     }
-    return $output;
+    return array($output);
   }
   
   $pth_program_data = null;
-  function pth_setProgramData(&$pd) {
-    $GLOBALS['pth_program_data'] = &$pd;
+  function pth_setProgramData($pd) {
+    $GLOBALS['pth_program_data'] = $pd;
   }
   
-  function &pth_getProgramData() {
+  function pth_getProgramData() {
     return $GLOBALS['pth_program_data'];
   }
   
-  function pth_list_reverse(&$list) {
-    $length = count($list);
+  function pth_list_reverse($list) {
+    $length = count($list[0]);
     for ($i = intval($length / 2) - 1; $i >= 0; --$i) {
       $j = $length - $i - 1;
-      $refLeft = is_array($list[$i]);
-      $refRight = is_array($list[$j]);
-      if ($refLeft) $t = &$list[$i]; else $t = $list[$i];
-      if ($refRight) $list[$i] = &$list[$j]; else $list[$i] = $list[$j];
-      if ($refLeft) $list[$j] = &$t; else $list[$j] = $t;
+      $t = $list[0][$i];
+      $list[0][$i] = $list[0][$j];
+      $list[0][$j] = $t;
     }
   }
   
-  function pth_getHttpRequest(&$stringOut) {
-    $stringOut[0] = $_SERVER['REQUEST_METHOD'];
-    $stringOut[1] = $_SERVER['REQUEST_URI'];
-    $stringOut[2] = $_SERVER['REMOTE_HOST'];
+  function pth_getHttpRequest($stringOut) {
+    $stringOut[0][0] = $_SERVER['REQUEST_METHOD'];
+    $stringOut[0][1] = $_SERVER['REQUEST_URI'];
+    $stringOut[0][2] = $_SERVER['REMOTE_HOST'];
   }
 ?>
