15ea2a23befd198320a6fd0bd1f17aae0df15c5e
Blake O'Hare <mspaintmaestro@gmail.com>
2016-07-31 17:38:27 -0700

    Finish implementing new ImageResources library for JavaScript
    Uses simple Core.sleep(t) instead of implementing its own VM suspension mechanism.
    
    This will briefly break the implementation on other platforms as I noticed I wasn't returning an error flag on the asynchronous ImageLoader, which is now fixed, but only for JS.
    Will go through and add the change to other platforms next.

--- Resources/game-javascript/images2.js
+++ Resources/game-javascript/images2.js
@@ -1,70 +1,32 @@
 ﻿
 C$imageresources = 1;
 
-C$imageresources$imageLoadAsync = function (filename, nativeData, imageLoaderNativeData) {
-	C$imageresources$imageLoadAsyncImpl(filename, nativeData, imageLoaderNativeData, false, 0);
-};
-
-C$imageresources$imageLoaderError = function (isSync, statusOut, execId) {
-	imageLoaderNativeData[isSync ? 0 : 2] = 2;
-
-	if (isSync) {
-		statusOut[0] = C$common$getBool(false);
-		C$imageresources$restartThread(execId);
-	} else {
-		statusOut[2] = 2;
-	}
-};
-
-C$imageresources$imageLoadAsyncImpl = function (filename, nativeData, imageLoaderNativeData, isActuallySync, executionContextId) {
+C$imageresources$imageLoad = function (filename, nativeData, imageLoaderNativeData) {
 	
 	var image = new Image();
 	var statusOut = imageLoaderNativeData;
 
 	image.onerror = function () {
-		C$imageresources$imageLoaderError(isActuallySync, imageLoaderNativeData, executionContextId);
+	    imageLoaderNativeData[2] = 2;
 	};
 
 	image.onload = function () {
-		if (isActuallySync && !statusOut[0][1]) { // status out is a list of Value bools
-			// The timeout handler has already run and marked this as not loaded and yielded back to the VM.
-            // It's too late for this handler to do anything.
-			return;
-		}
-
 		var w = image.width;
 		var h = image.height;
 		if (w < 1 || h < 1) { // another possible error case
-			C$imageresources$imageLoaderError(isActuallySync, imageLoaderNativeData, executionContextId);
+		    imageLoaderNativeData[2] = 2;
 		} else {
 			var canvas = C$imageresources$generateNativeBitmapOfSize(w, h);
 			var ctx = canvas.getContext('2d');
-			ctx.drawImage(img1, 0, 0);
+			ctx.drawImage(image, 0, 0);
 			nativeData[0] = canvas;
 			nativeData[1] = w;
 			nativeData[2] = h;
-			if (isActuallySync) {
-				statusOut[0] = C$common$getBool(true);
-			} else {
-				imageLoaderNativeData[2] = 1;
-			}
-
-			if (isActuallySync) {
-				C$imageresources$restartThread(executionContextId);
-			}
+			imageLoaderNativeData[2] = 1;
 		}
 	};
 
-	image.src = C$common$jsFilePrefix + filename;
-};
-
-C$imageresources$restartThread = function (executionContextId) {
-	C$game$runInterpreter(executionContextId);
-};
-
-
-C$imageresources$imageLoadSync = function (filename, nativeData, statusOut, executionContextId) {
-	C$imageresources$imageLoadAsyncImpl(filename, nativeData, statusOut, true, executionContextId);
+	image.src = C$common$jsFilePrefix + 'resources/images/' + filename;
 };
 
 C$imageresources$getImageResourceManifest = function () {
@@ -74,15 +36,9 @@ C$imageresources$getImageResourceManifest = function () {
 };
 
 C$imageresources$generateNativeBitmapOfSize = function (width, height) {
-	C$images$temp_image.innerHTML = '<canvas id="temp_image_canvas"></canvas>';
-	var canvas = C$common$getElement('temp_image_canvas');
+    var canvas = document.createElement('canvas');
 	canvas.width = width;
 	canvas.height = height;
-	var ctx = canvas.getContext('2d');
-	C$images$temp_image.innerHTML = '';
-	//return [canvas, ctx];
-
-	// TODO: make sure this works and then get rid of the ctx stuff.
 	return canvas;
 };
 
@@ -91,5 +47,5 @@ C$imageresources$imageResourceBlitImage = function (target, source, targetX, tar
 };
 
 C$imageresources$checkLoaderIsDone = function (imageLoaderNativeData, nativeImageDataNativeData, output) {
-	output[0] = (imageLoaderNativeData[2] != 0) ? v_VALUE_TRUE : v_VALUE_FALSE;
+    output[0] = v_buildInteger(imageLoaderNativeData[2]);
 };
