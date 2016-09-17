860c208f68eb7afbea7cb873c9c255f44a8e987e
Blake O'Hare <mspaintmaestro@gmail.com>
2016-09-17 11:17:24 -0700

    Get Xamarin Android projects building again. Image loaders behave like they're working but the renderer is currently stubbed out, so won't know for sure until that's done. Either way, this is a good check-in point.

---
+++ Libraries/ImageResources/resources/csharp/AndroidBitmap.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+namespace %%%PROJECT_ID%%%.Library.ImageResources
+{
+    public class AndroidBitmap
+    {
+        private Android.Graphics.Bitmap bmp;
+        private Android.Graphics.Canvas canvas;
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public AndroidBitmap(int width, int height)
+        {
+            this.bmp = Android.Graphics.Bitmap.CreateBitmap(
+                width,
+                height,
+                Android.Graphics.Bitmap.Config.Argb4444);
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public AndroidBitmap(Android.Graphics.Bitmap nativeBitmap)
+        {
+            this.bmp = nativeBitmap;
+            this.Width = nativeBitmap.Width;
+            this.Height = nativeBitmap.Height;
+        }
+
+        public void Blit(
+            object otherBmpObj,
+            int targetX,
+            int targetY,
+            int sourceX,
+            int sourceY,
+            int width,
+            int height)
+        {
+            AndroidBitmap otherBmp = (AndroidBitmap)otherBmpObj;
+            this.GetCanvas().DrawBitmap(otherBmp.bmp, targetX, targetY, null);
+        }
+
+        public Android.Graphics.Canvas GetCanvas()
+        {
+            if (this.canvas == null)
+            {
+                this.canvas = new Android.Graphics.Canvas(this.bmp);
+            }
+            return this.canvas;
+        }
+    }
+}
