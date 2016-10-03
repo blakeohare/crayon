ad532072d704ee1a09eee67d288cfe647e47790c
Blake O'Hare <mspaintmaestro@gmail.com>
2016-10-02 19:01:56 -0700

    Nori: Window main menu bar!
    This is just the basics. No event handlers yet for clicking on things or support for shortcut keys or checkboxes, enabling options, etc.
    Still issues with layout, but that's more related to the lack of window resizing feedback into the platform-agnostic renderer.

--- Libraries/Nori/resources/csharp-winforms/WindowUtil.cs
+++ Libraries/Nori/resources/csharp-winforms/WindowUtil.cs
@@ -1,16 +1,40 @@
+using System.Windows.Forms;
+
 namespace %%%PROJECT_ID%%%.Library.Nori
 {
     internal static class WindowUtil
     {
-        private class NoriWindow : System.Windows.Forms.Form
+        private class NoriWindow : Form
         {
             public int BackgroundExecutionContextId { get; set; }
             public bool IsBlocking { get; set; }
             public Value OnLoadFunctionPointer { get; set; }
             public Value RenderFunctionPointer { get; set; }
 
+            public bool MenuShown { get; set; }
+            public Panel MenuHost { get; set; }
+            public Panel ContentHost { get; set; }
+
             public NoriWindow() : base()
             {
+                this.MenuShown = false;
+                this.MenuHost = new Panel()
+                {
+                    Width = this.ClientSize.Width,
+                    Height = 0,
+                };
+                this.ContentHost = new Panel()
+                {
+                    Width = this.ClientSize.Width,
+                    Height = this.ClientSize.Height,
+                };
+
+                this.MainMenuStrip = new MenuStrip();
+                this.MenuHost.Controls.Add(this.MainMenuStrip);
+
+                this.Controls.Add(this.MenuHost);
+                this.Controls.Add(this.ContentHost);
+
                 this.Load += (sender, e) => { this.LoadHandler(); };
                 this.FormClosed += (sender, e) => { this.PostCloseHandler(); };
                 this.ClientSizeChanged += (sender, e) => { this.SizeChangedHandler(this.ClientSize.Width, this.ClientSize.Height); };
@@ -39,15 +63,44 @@ namespace %%%PROJECT_ID%%%.Library.Nori
             private void SizeChangedHandler(int newWidth, int newHeight)
             {
                 // TODO: update width and height properties
-                TranslationHelper.RunInterpreter(this.RenderFunctionPointer);
+                this.RunRenderer();
+            }
+
+            public void RunRenderer()
+            {
+                bool mainMenuPresent = this.MainMenuStrip != null;
+                int width = this.ClientSize.Width;
+                int height = this.ClientSize.Height;
+                if (this.MenuShown)
+                {
+                    int menuHeight = this.MainMenuStrip.Height;
+                    this.MenuHost.Width = width;
+                    this.MenuHost.Height = menuHeight;
+                    this.ContentHost.Width = width;
+                    this.ContentHost.Height = height - menuHeight;
+                    this.ContentHost.Location = new System.Drawing.Point(0, menuHeight);
+                }
+                else
+                {
+                    this.MenuHost.Width = 0;
+                    this.MenuHost.Height = 0;
+                    this.ContentHost.Width = width;
+                    this.ContentHost.Height = height;
+                    this.ContentHost.Location = new System.Drawing.Point(0, 0);
+                }
+
+                if (this.RenderFunctionPointer != null)
+                {
+                    TranslationHelper.RunInterpreter(this.RenderFunctionPointer);
+                }
             }
         }
 
         public static void InstaniateWindow(object[] windowNativeData, object[] uiBoxNativeData)
         {
             NoriWindow window = new NoriWindow();
-            System.Windows.Forms.Panel uiBoxPanel = new System.Windows.Forms.Panel();
-            window.Controls.Add(uiBoxPanel);
+            Panel uiBoxPanel = new System.Windows.Forms.Panel();
+            window.ContentHost.Controls.Add(uiBoxPanel);
             windowNativeData[0] = window;
             uiBoxNativeData[0] = uiBoxPanel;
         }
@@ -77,6 +130,50 @@ namespace %%%PROJECT_ID%%%.Library.Nori
             {
                 window.Show();
             }
+        }
+
+        public static void UpdateMenu(object windowObj, object[] menuData)
+        {
+            NoriWindow window = (NoriWindow)windowObj;
+            MenuStrip strip = window.MainMenuStrip;
+            window.MainMenuStrip.Items.Clear();
+            // TODO: suspend layout.
+
+            window.MenuShown = menuData != null && menuData.Length > 0;
+            if (window.MenuShown)
+            {
+                for (int i = 0; i < menuData.Length; ++i)
+                {
+                    object[] menuItem = (object[])menuData[i];
+                    if ((bool)menuItem[0])
+                    {
+                        strip.Items.Add(GenerateMenuItem(menuItem));
+                    }
+                }
+            }
+
+            window.RunRenderer();
+        }
+
+        private static ToolStripItem GenerateMenuItem(object[] menuItem)
+        {
+            if (!(bool)menuItem[0])
+            {
+                return new ToolStripSeparator();
+            }
+
+            string name = (string)menuItem[1];
+            ToolStripMenuItem item = new ToolStripMenuItem(name);
+            if (menuItem[5] != null)
+            {
+                object[] children = (object[])menuItem[5];
+                for (int i = 0; i < children.Length; ++i)
+                {
+                    object[] child = (object[])children[i];
+                    item.DropDownItems.Add(GenerateMenuItem(child));
+                }
+            }
+            return item;
         }
     }
 }
