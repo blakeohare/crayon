772558f774123a65a83b05744d891c5008ab3874
Blake O'Hare <mspaintmaestro@gmail.com>
2016-11-12 21:47:01 -0500

    Add Gosu support for some user events.

--- Resources/game-ruby-gosu/gameWindow.rb
+++ Resources/game-ruby-gosu/gameWindow.rb
@@ -15,7 +15,9 @@ def libGame_setWindowTitle(title)
 end
 
 def libGame_getEventsRaw
-	return []
+	output = @userEvents
+	@userEvents = []
+	return output
 end
 
 def libGame_clockTick
@@ -32,13 +34,22 @@ class GameWindow < Gosu::Window
 		@userEvents = []
 		@execId = execId
 		@stopExecution = false
+		@mouse_int_x = -1
+		@mouse_int_y = -1
 	end
 
 	def setCaption(title)
 		self.caption = title
 	end
 
+	def needs_cursor?
+		true
+	end
+
 	def update
+
+		self.update_mouse_coords
+
 		status = v_runInterpreter(@execId)
 		if status != 2
 			self.close	
@@ -54,6 +65,49 @@ class GameWindow < Gosu::Window
 		@renderEventsLength = eventsLength
 		@renderImages = images
 		@renderTextChars = textChars
+	end
+
+	def button_down(id)
+		self.key_toggle(id, true)
+	end
+
+	def button_up(id)
+		self.key_toggle(id, false)
+	end
+
+	def update_mouse_coords
+		# There are no mouse events triggered on movement, just polling of the current location
+		new_mouse_x = self.mouse_x.to_i
+		new_mouse_y = self.mouse_y.to_i
+		if (new_mouse_x != @mouse_int_x || new_mouse_y != @mouse_int_y)
+			@mouse_int_x = new_mouse_x
+			@mouse_int_y = new_mouse_y
+			@userEvents.push(v_buildRelayObj(32, new_mouse_x, new_mouse_y, 0, 0, nil))
+		end
+	end
+
+	def key_toggle(id, isDown)
+		case id
+			when Gosu::MsLeft, Gosu::MsRight
+				code = 32
+				code += 1 if !isDown
+				code += 2 if id == Gosu::MsRight
+				self.update_mouse_coords
+				@userEvents.push(v_buildRelayObj(code, @mouse_int_x, @mouse_int_y, 0, 0, nil))
+				
+
+			else
+				code = -1
+
+				case id
+					when Gosu::KbEscape then code = 27
+				end
+
+				if code > -1
+					@userEvents.push(v_buildRelayObj(isDown ? 16 : 17, code, 0, 0, 0, nil))
+				end
+		end
+
 	end
 
 end
