28725cac9819dca4e37b34465395695627b50625
Blake O'Hare <mspaintmaestro@gmail.com>
2017-03-18 15:04:29 -0700

    Stub out JavaScript platform modules.

---
+++ Compiler/GameJavaScript/PlatformImpl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common;
+using Pastel.Nodes;
+using Platform;
+
+namespace GameJavaScript
+{
+    public class PlatformImpl : AbstractPlatform
+    {
+        public override string Name { get { return "game-javascript-html5-cbx"; } }
+        public override string InheritsFrom { get { return "lang-javascript"; } }
+        public override string NL { get { return "\n"; } }
+
+        public override IDictionary<string, object> GetConstantFlags()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<string, FileOutput> Export(Dictionary<string, object[]> executablesPerCompilationUnit, object[] structDefinitions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<string, FileOutput> ExportProject(IList<VariableDeclaration> globals, IList<StructDefinition> structDefinitions, IList<FunctionDefinition> functionDefinitions, IList<LibraryForExport> libraries, ResourceDatabase resourceDatabase, Options options, ILibraryNativeInvocationTranslatorProvider libraryNativeInvocationTranslatorProviderForPlatform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GenerateCodeForStruct(StructDefinition structDef)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GenerateCodeForFunction(AbstractTranslator translator, FunctionDefinition funcDef)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GenerateCodeForGlobalsDefinitions(AbstractTranslator translator, IList<VariableDeclaration> globals)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<string, string> GenerateReplacementDictionary(Options options, ResourceDatabase resDb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
