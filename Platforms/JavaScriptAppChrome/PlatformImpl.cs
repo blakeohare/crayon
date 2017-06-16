b8ad664bae8aff36ac244d888cfa90bf2b1d0c29
Blake O'Hare <mspaintmaestro@gmail.com>
2017-06-16 22:24:32 +0900

    Initial checkin for Chrome Web App project type.

---
+++ Platforms/JavaScriptAppChrome/PlatformImpl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Pastel.Nodes;
+using Platform;
+
+namespace JavaScriptAppChrome
+{
+    public class PlatformImpl : Platform.AbstractPlatform
+    {
+        public override string InheritsFrom { get { return "lang-javascript"; } }
+        public override string Name { get { return "javascript-app-chrome"; } }
+        public override string NL { get { return "\n"; } }
+
+        public PlatformImpl()
+        {
+            this.Translator = new JavaScriptAppChromeTranslator(this);
+        }
+
+        public override Dictionary<string, FileOutput> ExportProject(
+            IList<VariableDeclaration> globals,
+            IList<StructDefinition> structDefinitions,
+            IList<FunctionDefinition> functionDefinitions,
+            IList<LibraryForExport> libraries,
+            ResourceDatabase resourceDatabase,
+            Options options,
+            ILibraryNativeInvocationTranslatorProvider libraryNativeInvocationTranslatorProviderForPlatform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Dictionary<string, FileOutput> ExportStandaloneVm(IList<VariableDeclaration> globals, IList<StructDefinition> structDefinitions, IList<FunctionDefinition> functionDefinitions, IList<LibraryForExport> everyLibrary, ILibraryNativeInvocationTranslatorProvider libraryNativeInvocationTranslatorProviderForPlatform)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GenerateCodeForFunction(AbstractTranslator translator, FunctionDefinition funcDef)
+        {
+            return this.ParentPlatform.GenerateCodeForFunction(translator, funcDef);
+        }
+
+        public override string GenerateCodeForGlobalsDefinitions(AbstractTranslator translator, IList<VariableDeclaration> globals)
+        {
+            return this.ParentPlatform.GenerateCodeForGlobalsDefinitions(translator, globals);
+        }
+
+        public override string GenerateCodeForStruct(StructDefinition structDef)
+        {
+            return this.ParentPlatform.GenerateCodeForStruct(structDef);
+        }
+
+        public override Dictionary<string, string> GenerateReplacementDictionary(Options options, ResourceDatabase resDb)
+        {
+            return this.ParentPlatform.GenerateReplacementDictionary(options, resDb);
+        }
+
+        public override IDictionary<string, object> GetConstantFlags()
+        {
+            return this.ParentPlatform.GetConstantFlags();
+        }
+    }
+}
