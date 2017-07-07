81260b8d3abb9a8c5bb33fa3711a558b8f61fd3c
Blake O'Hare <mspaintmaestro@gmail.com>
2017-07-07 19:30:43 +0900

    Text rendering technically works for system fonts in Python and Java.

---
+++ Libraries/Graphics2DText/resources/java/Grahpics2DTextHelper.java
@@ -0,0 +1,71 @@
+package org.crayonlang.libraries.graphics2dtext;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import org.crayonlang.interpreter.structs.*;
+
+final class Graphics2DTextHelper {
+	
+	private Graphics2DTextHelper() { }
+	
+	public static Object createNativeFont(int fontType, int fontClass, String fontId, int size, boolean isBold, boolean isItalic) {
+		if (fontType == 3) { // system font
+			int style = Font.PLAIN;
+			if (isBold) style |= Font.BOLD;
+			if (isItalic) style |= Font.ITALIC;
+			return new Font(fontId, style, size);
+		} else {
+			throw new RuntimeException("Not implemented.");
+		}
+	}
+	
+	public static void addAll(ArrayList<Value> values, Value... toAdd) {
+		for (Value value : toAdd) {
+			values.add(value);
+		}
+	}
+	
+	public static boolean isFontResourceAvailable(String path) {
+		throw new RuntimeException("Not implemented.");
+	}
+	
+	public static boolean isSystemFontAvailable(String name) {
+		String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+		for (String fontName : fontNames) {
+			if (fontName.equals(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static final BufferedImage DUMMY_IMAGE = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+	
+	public static Object renderText(
+		int[] sizeOut,
+		Object fontObj,
+		int red,
+		int green,
+		int blue,
+		String text) {
+		
+		Font font = (Font) fontObj;
+		FontMetrics fontMetrics = DUMMY_IMAGE.createGraphics().getFontMetrics(font);
+		int width = fontMetrics.stringWidth(text);
+		int height = fontMetrics.getHeight();
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = image.createGraphics();
+		Color color = new Color(red, green, blue);
+		g.setColor(color);
+		g.setFont(font);
+		g.drawString(text, 0, height);
+		sizeOut[0] = width;
+		sizeOut[1] = height;
+		return image;
+	}
+}
