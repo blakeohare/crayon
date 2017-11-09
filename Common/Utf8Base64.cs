06a9435467b97a8df36d65ed8ca78cf13dcd5fa0
Blake O'Hare <mspaintmaestro@gmail.com>
2017-11-08 19:05:24 -0800

    Add a Base64 string to UTF8 conversion utility class.

---
+++ Common/Utf8Base64.cs
@@ -0,0 +1,19 @@
+﻿namespace Common
+{
+    public static class Utf8Base64
+    {
+        public static string ToBase64(string value)
+        {
+            byte[] utf8Bytes = System.Text.Encoding.UTF8.GetBytes(value);
+            string encodedValue = System.Convert.ToBase64String(utf8Bytes);
+            return encodedValue;
+        }
+
+        public static string FromBase64(string encodedValue)
+        {
+            byte[] utf8Bytes = System.Convert.FromBase64String(encodedValue);
+            string value = System.Text.Encoding.UTF8.GetString(utf8Bytes);
+            return value;
+        }
+    }
+}
