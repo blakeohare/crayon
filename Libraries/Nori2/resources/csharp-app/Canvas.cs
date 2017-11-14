0423484b4b24efe4af43ccaa0a0a9956cfd73029
Blake O'Hare <mspaintmaestro@gmail.com>
2017-11-14 18:50:42 +0900

    Nori is kind of working.

---
+++ Libraries/Nori2/resources/csharp-app/Canvas.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interpreter.Libraries.Nori2
+{
+    public class Canvas : System.Windows.Forms.Panel
+    {
+        public void SetPosition(int x, int y, int width, int height)
+        {
+            this.Location = new System.Drawing.Point(x, y);
+            this.Size = new System.Drawing.Size(width, height);
+        }
+
+        public System.Windows.Forms.Control[] Children
+        {
+            get { return NoriHelper.GetControls(this.Controls); }
+        }
+
+        public override string ToString()
+        {
+            return "Canvas: <" + this.Location.X + ", " + this.Location.Y + ", " + this.Size.Width + ", " + this.Size.Height + ">";
+        }
+    }
+}
--- Libraries/Nori2/resources/csharp-app/NoriHelper.cs
+++ Libraries/Nori2/resources/csharp-app/NoriHelper.cs
@@ -6,7 +6,7 @@ namespace Interpreter.Libraries.Nori2
 {
     public static class NoriHelper
     {
-        public static void AddChildToParent(object parent, object child)
+        public static void AddChildToParent(object child, object parent)
         {
             ((System.Windows.Forms.Panel)parent).Controls.Add((System.Windows.Forms.Control)child);
         }
@@ -16,55 +16,72 @@ namespace Interpreter.Libraries.Nori2
             throw new NotImplementedException();
         }
 
-        public static void EnsureParentLinkOrder(object window, object[] children)
+        public static void EnsureParentLinkOrder(object parent, object[] children)
         {
+            System.Windows.Forms.Control.ControlCollection actualCollection = ((System.Windows.Forms.Control)parent).Controls;
+            System.Windows.Forms.Control[] actualOrder = GetControls(actualCollection);
+
+            int perfectMatchUntil = 0;
+            if (actualOrder.Length == children.Length)
+            {
+                for (int i = 0; i < children.Length; ++i)
+                {
+                    if (children[i] == actualOrder[i])
+                    {
+                        perfectMatchUntil = i + 1;
+                    }
+                }
+                if (perfectMatchUntil == children.Length)
+                {
+                    return;
+                }
+            }
             throw new NotImplementedException();
         }
 
-        private static readonly System.Drawing.Point DEFAULT_LOCATION = new System.Drawing.Point();
-        private static readonly System.Drawing.Size DEFAULT_SIZE = new System.Drawing.Size();
-        private static readonly System.Drawing.Color DEFAULT_COLOR = System.Drawing.Color.Transparent;
-
         public static object InstantiateElement(int type, object[] properties)
         {
-            System.Drawing.Point location = DEFAULT_LOCATION;
-            System.Drawing.Size size = DEFAULT_SIZE;
-            System.Drawing.Color color = DEFAULT_COLOR;
+            int left = 0;
+            int top = 0;
+            int width = 0;
+            int height = 0;
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            int alpha = 0;
+
             if (properties[0] != null)
             {
-                location = new System.Drawing.Point((int)properties[0], (int)properties[1]);
-                size = new System.Drawing.Size((int)properties[4], (int)properties[5]);
+                left = (int)properties[0];
+                top = (int)properties[1];
+                width = (int)properties[4];
+                height = (int)properties[5];
             }
             if (properties[16] != null)
             {
                 List<Value> colors = (List<Value>)properties[16];
-                int red = (int)colors[0].internalValue;
-                int green = (int)colors[1].internalValue;
-                int blue = (int)colors[2].internalValue;
-                int alpha = (int)colors[3].internalValue;
-                color = System.Drawing.Color.FromArgb(alpha, red, green, blue);
+                red = (int)colors[0].internalValue;
+                green = (int)colors[1].internalValue;
+                blue = (int)colors[2].internalValue;
+                alpha = (int)colors[3].internalValue;
             }
 
             switch (type)
             {
                 case 1: // Rectangle
-                    System.Windows.Forms.Panel rect = new System.Windows.Forms.Panel();
-                    rect.Location = location;
-                    rect.Size = size;
-                    rect.BackColor = color;
+                    Rectangle rect = new Rectangle();
+                    rect.SetPosition(left, top, width, height);
+                    rect.SetColor(red, green, blue, alpha);
                     return rect;
 
                 case 2: // Canvas
-                    System.Windows.Forms.Panel canvas = new System.Windows.Forms.Panel();
-                    canvas.Location = location;
-                    canvas.Size = size;
+                    Canvas canvas = new Canvas();
+                    canvas.SetPosition(left, top, width, height);
                     return canvas;
 
                 case 3: // ScrollPanel
-                    System.Windows.Forms.Panel scrollPanel = new System.Windows.Forms.Panel();
-                    scrollPanel.AutoScroll = true;
-                    scrollPanel.Location = location;
-                    scrollPanel.Size = size;
+                    ScrollPanel scrollPanel = new ScrollPanel();
+                    scrollPanel.SetPosition(left, top, width, height);
                     return scrollPanel;
 
                 default:
@@ -94,10 +111,42 @@ namespace Interpreter.Libraries.Nori2
             throw new NotImplementedException();
         }
 
-        public static void ShowWindow(object window, object[] ignored)
+        public static void ShowWindow(object window, object[] ignored, object rootElement)
         {
             System.Windows.Forms.Form form = (System.Windows.Forms.Form)window;
+            form.Controls.Add((System.Windows.Forms.Panel)rootElement);
             form.ShowDialog();
+        }
+
+        public static System.Windows.Forms.Control[] GetControls(System.Windows.Forms.Control.ControlCollection cc)
+        {
+            List<System.Windows.Forms.Control> output = new List<System.Windows.Forms.Control>();
+            foreach (System.Windows.Forms.Control c in cc)
+            {
+                output.Add(c);
+            }
+            return output.ToArray();
+        }
+
+        public static void UpdateLayout(object element, int typeId, int x, int y, int width, int height)
+        {
+            switch (typeId)
+            {
+                case 1:
+                    Rectangle r = (Rectangle)element;
+                    r.Location = new System.Drawing.Point(x, y);
+                    r.Size = new System.Drawing.Size(width, height);
+                    break;
+
+                case 3:
+                    ScrollPanel sp = (ScrollPanel)element;
+                    sp.Location = new System.Drawing.Point(x, y);
+                    sp.Size = new System.Drawing.Size(width, height);
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
