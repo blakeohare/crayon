3039b28e9105c329a311536ba8cd7df6afb50a01
Blake O'Hare <mspaintmaestro@gmail.com>
2017-11-15 21:07:14 +0900

    Stub out Java implementation of Nori

---
+++ Libraries/Nori2/resources/java-app/NoriHelper.java
@@ -0,0 +1,42 @@
+package org.crayonlang.libraries.nori2;
+
+final class NoriHelper {
+
+	private NoriHelper() {}
+
+	public static void addChildToParent(object child, object parent) {
+		throw new RuntimeException();
+	}
+
+	public static void closeWindow(object window) {
+		throw new RuntimeException();
+	}
+
+	public static void ensureParentLinkOrder(object parent, object[] children) {
+		throw new RuntimeException();
+	}
+
+	public static void instantiateElement(int type, object[] properties) {
+		throw new RuntimeException();
+	}
+
+	public static void instantiateWindow(object[] properties) {
+		throw new RuntimeException();
+	}
+
+	public static void invalidateElementProperty(int type, object element, int key, object value) {
+		throw new RuntimeException();
+	}
+
+	public static void invalidateWindowProperty(object window, int key, object value) {
+		throw new RuntimeException();
+	}
+
+	public static void showWindow(object window, object[] ignored, object rootElement) {
+		throw new RuntimeException();
+	}
+
+	public static void updateLayout(object element, int typeId, int x, int y, int width, int height) {
+		throw new RuntimeException();
+	}
+}
