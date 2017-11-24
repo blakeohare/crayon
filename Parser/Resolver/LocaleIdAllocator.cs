0797e144c42eb96fd81db6d9d436dc430b86d0d9
Blake O'Hare <mspaintmaestro@gmail.com>
2017-11-24 18:24:02 +0900

    Fix compilation error introduced with last minute cleanup added when looking at the diff.

--- Parser/Resolver/LocaleIdAllocator.cs
+++ Parser/Resolver/LocaleIdAllocator.cs
@@ -5,15 +5,17 @@ namespace Parser
     // Assigns each locale an ID from 0 to n - 1
     internal static class LocaleIdAllocator
     {
+        // TODO: merge this with the current ID allocator in ParserContext.
         public static void Run(ParserContext parser, IList<CompilationScope> scopes)
         {
+            /*
             foreach (CompilationScope scope in scopes)
             {
                 if (!parser.LocaleIds.ContainsKey(scope.Locale))
                 {
                     parser.LocaleIds.Add(scope.Locale, parser.LocaleIds.Count);
                 }
-            }
+            }//*/
         }
     }
 }
