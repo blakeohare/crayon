b649c01674a5240c77b93b8986c7b04907886940
Blake O'Hare <mspaintmaestro@gmail.com>
2018-02-18 12:41:47 +0900

    Disable GIF recorder for OSX

--- Libraries/GameGifCap/resources/csharp-app/GameGifCapHelper.cs
+++ Libraries/GameGifCap/resources/csharp-app/GameGifCapHelper.cs
@@ -2,6 +2,15 @@ namespace Interpreter.Libraries.GameGifCap
 {
     public static class GameGifCapHelper
     {
+        public static bool IsSupported()
+        {
+#if WINDOWS
+            return true;
+#else
+            return false;
+#endif
+        }
+
         public static int SaveToDisk(object gifRecContextObj, string path)
         {
             return ((GifRecorderContext)gifRecContextObj).Finish(path);
