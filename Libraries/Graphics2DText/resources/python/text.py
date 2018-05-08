83ac0ba3edfd881ef6396625ddb544aa39d39474
Blake O'Hare <mspaintmaestro@gmail.com>
2018-05-08 20:53:38 +0900

    Python export: Fix Graphics2DText and Gamepad library missing references to PyGame

--- Libraries/Graphics2DText/resources/python/text.py
+++ Libraries/Graphics2DText/resources/python/text.py
@@ -2,6 +2,7 @@
 
 def lib_graphics2dtext_createNativeFont(fontType, fontClass, fontPath, fontSize, isBold, isItalic):
 	fontSize = fontSize * 7 / 5 # adjust to match standard sizes
+	pygame = GetPyGameReferenceWorkaround()
 	if fontType == 0: # default
 		raise Exception("Not implemented: default font by class")
 	elif fontType == 1: # resource
@@ -24,6 +25,7 @@ def lib_graphics2dtext_name_canonicalizer(name):
 	return ''.join(output)
 
 def lib_graphics2dtext_isSystemFontAvailable(name):
+	pygame = GetPyGameReferenceWorkaround()
 	return lib_graphics2dtext_name_canonicalizer(name) in pygame.font.get_fonts()
 
 def lib_graphics2dtext_renderText(sizeOut, nativeFont, red, green, blue, text):
@@ -32,6 +34,7 @@ def lib_graphics2dtext_renderText(sizeOut, nativeFont, red, green, blue, text):
 	side_margin = height // 32
 	top_margin = height // 8 + 4
 	bottom_margin = 4
+	pygame = GetPyGameReferenceWorkaround()
 	padded = pygame.Surface((width + side_margin * 2, height + bottom_margin + top_margin), pygame.SRCALPHA)
 	padded.blit(surface, (side_margin, top_margin))
 	width, height = padded.get_size()
