448e5e2034d8af8271aca54e61011102da11a62c
Blake O'Hare <mspaintmaestro@gmail.com>
2018-08-29 21:22:05 +0900

    Remove some game-specific files out of the Java-App platform and into the Game library resources.

--- Compiler/Platforms/JavaApp/PlatformImpl.cs
+++ Compiler/Platforms/JavaApp/PlatformImpl.cs
@@ -100,10 +100,6 @@ namespace JavaApp
             this.CopyResourceAsText(output, "src/org/crayonlang/interpreter/ResourceReader.java", "Resources/ResourceReader.txt", replacements);
             this.CopyResourceAsText(output, "src/org/crayonlang/interpreter/AwtTranslationHelper.java", "Resources/AwtTranslationHelper.txt", replacements);
 
-            // need to move these to library supplemental files
-            this.CopyResourceAsText(output, "src/org/crayonlang/interpreter/GameWindow.java", "Resources/GameWindow.txt", replacements);
-            this.CopyResourceAsText(output, "src/org/crayonlang/interpreter/RenderEngine.java", "Resources/RenderEngine.txt", replacements);
-
             this.CopyResourceAsText(output, srcPackagePath + "/Main.java", "Resources/Main.txt", replacements);
             this.CopyResourceAsText(output, "build.xml", "Resources/BuildXml.txt", replacements);
 
