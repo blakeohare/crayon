c9c655919ac6568f0790d13c4fb3b53320a43da1
Blake O'Hare <mspaintmaestro@gmail.com>
2018-09-09 15:50:28 +0900

    Put the local ID of the iterating variable for foreach loops into the iteration step instruction rather than pushing it onto the values stack.

--- Compiler/Exporter/ByteCode/Nodes/ForEachEncoder.cs
+++ Compiler/Exporter/ByteCode/Nodes/ForEachEncoder.cs
@@ -8,18 +8,17 @@ namespace Exporter.ByteCode.Nodes
         public static void Compile(ByteCodeCompiler bcc, ParserContext parser, ByteBuffer buffer, ForEachLoop forEachLoop)
         {
             buffer.Add(null, OpCode.LITERAL, parser.GetIntConstant(0));
-            buffer.Add(null, OpCode.LITERAL, parser.GetIntConstant(forEachLoop.IterationVariableId.ID));
             bcc.CompileExpression(parser, buffer, forEachLoop.IterationExpression, true);
             buffer.Add(forEachLoop.IterationExpression.FirstToken, OpCode.VERIFY_TYPE_IS_ITERABLE);
 
-            buffer.SetLastValueStackDepthOffset(3);
+            buffer.SetLastValueStackDepthOffset(2);
 
             ByteBuffer body = new ByteBuffer();
             ByteBuffer body2 = new ByteBuffer();
 
             bcc.Compile(parser, body2, forEachLoop.Code);
 
-            body.Add(forEachLoop.FirstToken, OpCode.ITERATION_STEP, body2.Size + 1);
+            body.Add(forEachLoop.FirstToken, OpCode.ITERATION_STEP, body2.Size + 1, forEachLoop.IterationVariableId.ID);
 
             body2.Add(null, OpCode.JUMP, -body2.Size - 2);
             body.Concat(body2);
@@ -29,9 +28,8 @@ namespace Exporter.ByteCode.Nodes
 
             buffer.Concat(body);
             buffer.Add(null, OpCode.POP); // list
-            buffer.Add(null, OpCode.POP); // var ID
             buffer.Add(null, OpCode.POP); // index
-            buffer.SetLastValueStackDepthOffset(-3);
+            buffer.SetLastValueStackDepthOffset(-2);
         }
     }
 }
