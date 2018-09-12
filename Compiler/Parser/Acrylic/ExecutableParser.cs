5b0bed6a58ad39b37db993e2125e59c6655acb97
Blake O'Hare <mspaintmaestro@gmail.com>
2018-09-12 11:38:48 +0900

    Use separate parsers for Acrylic (still stubs, though).

--- Compiler/Parser/Acrylic/ExecutableParser.cs
+++ Compiler/Parser/Acrylic/ExecutableParser.cs
@@ -5,6 +5,12 @@ namespace Parser.Acrylic
 {
     internal class ExecutableParser : IExecutableParser
     {
+        private ParserContext parser;
+        public ExecutableParser(ParserContext parser)
+        {
+            this.parser = parser;
+        }
+
         public Executable Parse(TokenStream tokens, bool simpleOnly, bool semicolonPresent, Node owner)
         {
             throw new System.NotImplementedException();
