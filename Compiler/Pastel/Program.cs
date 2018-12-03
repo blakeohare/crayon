3ca63e0937343a00f62275f57a1f6b088195ca08
Blake O'Hare <mspaintmaestro@gmail.com>
2018-12-03 12:17:47 +0900

    Delete some dead code in Pastel project.
    JSON parser is no longer being used.

--- Compiler/Pastel/Program.cs
+++ Compiler/Pastel/Program.cs
@@ -1,5 +1,4 @@
-﻿using SimpleJson;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -222,38 +221,6 @@ namespace Pastel
                 path = System.IO.Path.GetFullPath(path);
                 return System.IO.File.ReadAllText(path);
             }
-        }
-
-        public static bool ExportForTarget(string pastelProj, string targetName)
-        {
-            if (!System.IO.File.Exists(pastelProj)) return false;
-            string pastelProjTextRaw = System.IO.File.ReadAllText(pastelProj);
-            IDictionary<string, object> jsonDict = new JsonParser(pastelProjTextRaw).ParseAsDictionary();
-            JsonLookup jsonRoot = new JsonLookup(jsonDict);
-            JsonLookup jsonTarget = jsonRoot
-                .GetAsList("targets")
-                .OfType<IDictionary<string, object>>()
-                .Select(t => new JsonLookup(t))
-                .Where(t => t.GetAsString("name") == targetName)
-                .FirstOrDefault();
-            if (jsonTarget == null) return false;
-
-            string source = jsonTarget.GetAsString("source") ?? jsonRoot.GetAsString("source");
-            string output = jsonTarget.GetAsString("output") ?? jsonRoot.GetAsString("output");
-            string language = jsonTarget.GetAsString("language") ?? jsonRoot.GetAsString("language");
-
-            CodeLoader codeLoader = new CodeLoader(System.IO.Path.GetFileName(source));
-            string sourceDir = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(source));
-            PastelContext pastelContext = new PastelContext(sourceDir, language, codeLoader);
-
-            // TODO(pastel-split): this
-            Dictionary<string, string> extensibleFunctionTranslations = new Dictionary<string, string>();
-            List<ExtensibleFunction> extensibleFunctions = new List<ExtensibleFunction>();
-
-            // TODO(pastel-split): load constants from manifest
-            Dictionary<string, object> constants = new Dictionary<string, object>();
-
-            return true;
         }
     }
 }
