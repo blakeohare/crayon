549ab27019babf015efc749cf3d426ede32a1b9a
Blake O'Hare <mspaintmaestro@gmail.com>
2018-12-11 15:23:33 -0500

    Add support for C# style foreach loops in Acrylic when C# compat mode is enabled.

--- Compiler/Parser/AbstractExecutableParser.cs
+++ Compiler/Parser/AbstractExecutableParser.cs
@@ -66,6 +66,8 @@ namespace Parser
                 if (value == this.parser.Keywords.BREAK) return this.ParseBreak(tokens, owner);
                 if (value == this.parser.Keywords.CONTINUE) return this.ParseContinue(tokens, owner);
                 if (value == this.parser.Keywords.THROW) return this.ParseThrow(tokens, owner);
+
+                if (this.parser.IsCSharpCompat && value == "foreach") return this.ParseForEachCSharp(tokens, owner);
             }
 
             Assignment assignmentExec = this.MaybeParseTypedVariableDeclaration(tokens, owner);
@@ -157,6 +159,21 @@ namespace Parser
                 IList<Executable> body = this.ParseBlock(tokens, false, owner);
                 return new ForLoop(forToken, parts.Item1, parts.Item2, parts.Item3, body, owner);
             }
+        }
+
+        private ForEachLoop ParseForEachCSharp(TokenStream tokens, Node owner)
+        {
+            if (!this.parser.IsCSharpCompat) throw new System.InvalidOperationException();
+
+            Token foreachToken = tokens.PopExpected("foreach");
+            tokens.PopExpected("(");
+            AType iteratorVariableType = this.parser.TypeParser.Parse(tokens);
+            Token iteratorVariable = tokens.PopWord();
+            tokens.PopExpected("in");
+            Expression listExpr = this.parser.ExpressionParser.Parse(tokens, owner);
+            tokens.PopExpected(")");
+            IList<Executable> body = this.ParseBlock(tokens, false, owner);
+            return new ForEachLoop(foreachToken, iteratorVariableType, iteratorVariable, listExpr, body, owner);
         }
 
         protected abstract System.Tuple<AType, Token> ParseForEachLoopIteratorVariable(TokenStream tokens, Node owner);
