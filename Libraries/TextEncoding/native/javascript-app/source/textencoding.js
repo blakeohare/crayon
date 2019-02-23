80367e769bc9dd69f9a30748bbbfd4ada78d12ef
Blake O'Hare <mspaintmaestro@gmail.com>
2019-02-23 16:45:26 -0500

    Finish(?) JS text encoding implementation.

--- Libraries/TextEncoding/native/javascript-app/source/textencoding.js
+++ Libraries/TextEncoding/native/javascript-app/source/textencoding.js
@@ -3,14 +3,20 @@ LIB$textencoding$bytesToText = function(bytes, format, strOut) {
 	var i;
 	var length = bytes.length;
 	var sb = [];
+	var a;
+	var b;
 	var c;
 	var cp;
 	var t;
+	var isBigEndian = format == 5 || format == 7;
 	switch (format) {
 		case 1:
 		case 2:
+			var isAscii = format == 1;
 			for (i = 0; i < length; ++i) {
-				String.fromCharCode(bytes[i]);
+				c = bytes[i];
+				if (isAscii && c > 127) return 1;
+				sb.push(String.fromCharCode(c));
 			}
 			break;
 		case 3:
@@ -51,43 +57,85 @@ LIB$textencoding$bytesToText = function(bytes, format, strOut) {
 					cp = (cp << 6) | (c & 0x3F);
 					i += 4;
 				}
-				sb.push(String.fromCharCode(c));
+				sb.push(String.fromCodePoint(cp));
 			}
 			break;
 		
 		case 4:
 		case 5:
-			var a;
-			var b;
-			var charPair = [0, 0];
-			var be = format == 5;
 			for (i = 0; i < length; i += 2) {
-				if (be) {
-					b = bytes[i];
-					a = bytes[i + 1];
-				} else {
+				if (isBigEndian) {
 					a = bytes[i];
 					b = bytes[i + 1];
+				} else {
+					a = bytes[i + 1];
+					b = bytes[i];
 				}
-				
+				c = (a << 8) | b;
+				if (c < 0xD800 || c > 0xDFFF) {
+					cp = c;
+				} else if (c < 0xD800 && c >= 0xDC00) {
+					return 1;
+				} else if (i + 3 >= length) {
+					return 1;
+				} else {
+					if (isBigEndian) {
+						a = bytes[i + 2];
+						b = bytes[i + 3];
+					} else {
+						a = bytes[i + 3];
+						b = bytes[i + 2];
+					}
+					b = (a << 8) | b;
+					a = c;
+					// a and b are now 16 bit words
+					if (b < 0xDC00 || b > 0xDFFF) return 1;
+					cp = (((a & 0x03FF) << 10) | (b & 0x03FF)) + 0x10000;
+					i += 2;
+				}
+				sb.push(String.fromCodePoint(cp));
 			}
-			
+			break;
+		
+		case 6:
+		case 7:
+			for (i = 0; i < length; i += 4) {
+				if (isBigEndian) {
+					cp = bytes[i + 3] | (bytes[i + 2] << 8) | (bytes[i + 1] << 16) | (bytes[i] << 24);
+				} else {
+					cp = bytes[i] | (bytes[i + 1] << 8) | (bytes[i + 2] << 16) | (bytes[i + 3] << 24);
+				}
+				sb.push(String.fromCodePoint(cp));
+			}
+			break;
 	}
 	
-	strOut = sb.join('');
+	strOut[0] = sb.join('');
 	return 0;
 };
 
+LIB$textencoding$stringToCodePointList = function(s) {
+	var codePoints = Array.from(s);
+	var length = codePoints.length;
+	var n;
+	for (var i = 0; i < length; ++i) {
+		n = codePoints[i].codePointAt(0);
+		codePoints[i] = n;
+	}
+	return codePoints;
+};
+
 LIB$textencoding$textToBytes = function(value, includeBom, format, byteList, positiveIntegers, intOut) {
-	var codePoints = [];
-	var length = value.length;
+	intOut[0] = 0;
+	var codePoints = LIB$textencoding$stringToCodePointList(value);
+	var length = codePoints.length;
 	var maxValue = 0;
 	var n = 0;
 	var i;
 	for (i = 0; i < length; ++i) {
-		n = value.charCodeAt(i);
-		if (n > maxValue) maxValue = n;
-		codePoints.push(n);
+		if (codePoints[i] > maxValue) {
+			maxValue = codePoints[i];
+		}
 	}
 	if (maxValue > 127 && format == 1) return 1;
 
@@ -109,8 +157,18 @@ LIB$textencoding$textToBytes = function(value, includeBom, format, byteList, pos
 			}
 		}
 		length = codePoints.length;
-		for (i = 0; i < length; ++i) {
-			
+		switch (format) {
+			case 3:
+				LIB$textencoding$codePointsToUtf8(codePoints, bytes);
+				break;
+			case 4:
+			case 5:
+				LIB$textencoding$codePointsToUtf16(codePoints, bytes, format == 5);
+				break;
+			case 6:
+			case 7:
+				LIB$textencoding$codePointsToUtf32(codePoints, bytes, format == 7);
+				break;
 		}
 	}
 	
@@ -124,10 +182,11 @@ LIB$textencoding$textToBytes = function(value, includeBom, format, byteList, pos
 LIB$textencoding$codePointsToUtf8 = function(points, buffer) {
 	var length = points.length;
 	var p;
-	for (var pIndex = 0; pIndex < length; ++i) {
+	for (var pIndex = 0; pIndex < length; ++pIndex) {
 		p = points[pIndex];
-		if (p < 0x80) buffer.push(p);
-		else if (p < 0x0800) {
+		if (p < 0x80) {
+			buffer.push(p);
+		} else if (p < 0x0800) {
 			buffer.push(0xC0 | ((p >> 6) & 0x1F));
 			buffer.push(0x80 | (p & 0x3F));
 		} else if (p < 0x10000) {
@@ -150,18 +209,16 @@ LIB$textencoding$codePointsToUtf16 = function(points, buffer, isBigEndian) {
 	var a;
 	var b;
 	
-	for (var pIndex = 0; pIndex < length; ++i) {
+	for (var pIndex = 0; pIndex < length; ++pIndex) {
 		p = points[pIndex];
 		if (p < 0x10000) {
 			if (isBigEndian) {
-				a = (p >> 8) & 255;
-				b = p & 255;
+				buffer.push((p >> 8) & 255);
+				buffer.push(p & 255);
 			} else {
-				b = (p >> 8) & 255;
-				a = p & 255;
+				buffer.push(p & 255);
+				buffer.push((p >> 8) & 255);
 			}
-			buffer.push(a);
-			buffer.push(b);
 		} else {
 			p -= 0x10000;
 			a = 0xD800 | ((p >> 10) & 0x03FF);
@@ -177,6 +234,26 @@ LIB$textencoding$codePointsToUtf16 = function(points, buffer, isBigEndian) {
 				buffer.push(b & 255);
 				buffer.push((b >> 8) & 255);
 			}
+		}
+	}
+};
+
+LIB$textencoding$codePointsToUtf32 = function(points, buffer, isBigEndian) {
+	var i = 0;
+	var length = points.length;
+	var p;
+	while (i < length) {
+		p = points[i++];
+		if (isBigEndian) {
+			buffer.push((p >> 24) & 255);
+			buffer.push((p >> 16) & 255);
+			buffer.push((p >> 8) & 255);
+			buffer.push(p & 255);
+		} else {
+			buffer.push(p & 255);
+			buffer.push((p >> 8) & 255);
+			buffer.push((p >> 16) & 255);
+			buffer.push((p >> 24) & 255);
 		}
 	}
 };
