5b5a1ccd0d8dd82069a2ceae12c3d7f74befb62b
Blake O'Hare <mspaintmaestro@gmail.com>
2019-04-11 19:58:03 -0700

    Update the copyright year in various places. Mostly */Properties/AssemblyInfo.cs
    I even found a lingering 2017!

--- Compiler/Crayon/Properties/AssemblyInfo.cs
+++ Compiler/Crayon/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Crayon")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
