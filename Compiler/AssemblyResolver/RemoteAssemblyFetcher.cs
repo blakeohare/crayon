258bd70e74276551f419e2974dd3fe25075d9a90
Blake O'Hare <mspaintmaestro@gmail.com>
2019-04-22 10:16:40 -0700

    Stub out some files for remote assembly fetching.

---
+++ Compiler/AssemblyResolver/RemoteAssemblyFetcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyResolver
+{
+    public enum FetchAssemblyStatus
+    {
+        INVALID_URL,
+        NO_CONNECTION,
+        SERVER_NOT_RESPONDING,
+        UNKNOWN_RESPONSE,
+        LIBRARY_NOT_FOUND,
+        SUCCESS,
+    }
+
+    internal class RemoteAssemblyFetcher
+    {
+        public FetchAssemblyStatus EnsureAssemblyFetched(string url)
+        {
+            RemoteAssemblyUrl structuredUrl = RemoteAssemblyUrl.FromUrl(url);
+            if (!structuredUrl.IsValid) return FetchAssemblyStatus.INVALID_URL;
+            throw new NotImplementedException();
+        }
+        
+    }
+}
---
+++ Compiler/AssemblyResolver/RemoteAssemblyManifest.cs
@@ -0,0 +1,21 @@
+﻿namespace AssemblyResolver
+{
+    internal class RemoteAssemblyManifest
+    {
+        private string directory = null;
+
+        public string Directory
+        {
+            get
+            {
+                if (this.directory == null)
+                {
+                    string appData = System.Environment.GetEnvironmentVariable("%APPDATA%");
+                    this.directory = System.IO.Path.Combine(appData, "Roaming", "Crayon", "libstore");
+                    Common.FileUtil.EnsureFolderExists(this.directory);
+                }
+                return this.directory;
+            }
+        }
+    }
+}
