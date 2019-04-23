ac6d568db1bcfc9537e34c0308f802570798276e
Blake O'Hare <mspaintmaestro@gmail.com>
2019-04-23 11:56:01 -0700

    VersionInfo was not updated.

--- Compiler/Common/VersionInfo.cs
+++ Compiler/Common/VersionInfo.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Common
+﻿namespace Common
 {
     public static class VersionInfo
     {
-        public const int VersionMajor = 0;
-        public const int VersionMinor = 2;
-        public const int VersionBuild = 1;
+        // 2.1.0
+        public const int VersionMajor = 2;
+        public const int VersionMinor = 1;
+        public const int VersionBuild = 0;
 
         public static string VersionString { get { return VersionMajor + "." + VersionMinor + "." + VersionBuild; } }
         public static int[] VersionArray { get { return new int[] { VersionMajor, VersionMinor, VersionBuild }; } }
