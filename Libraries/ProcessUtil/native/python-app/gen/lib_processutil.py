1046035241985144fa91b1791f491ec534c4fbaf
blakeohare <mspaintmaestro@gmail.com>
2019-05-17 19:12:54 -0700

    Stub out a ProcessUtil library for launching system processes.

---
+++ Libraries/ProcessUtil/native/python-app/gen/lib_processutil.py
@@ -0,0 +1,60 @@
+import base64
+import code.vm as VM
+import math
+import os
+import random
+import sys
+import time
+
+PST_StringBuffer16 = [None] * 16
+PST_IntBuffer16 = [0] * 16
+PST_FloatBuffer16 = [0.0] * 16
+PST_NoneListOfOne = [None]
+
+PST_StringType = type('')
+def PST_base64ToString(value):
+  u_value = base64.b64decode(value)
+  if type(u_value) == PST_StringType:
+    return u_value
+  return u_value.decode('utf8')
+
+def PST_isValidInteger(value):
+  if len(value) == 0: return False
+  if value[0] == '-': value = value[1:]
+  return value.isdigit()
+
+def PST_sortedCopyOfList(t):
+  t = t[:]
+  t.sort()
+  return t
+
+def PST_tryParseFloat(value, floatOut):
+  try:
+    floatOut[1] = float(value)
+    floatOut[0] = 1.0
+  except:
+    floatOut[0] = -1.0
+
+def PST_stringCheckSlice(haystack, i, needle):
+  return haystack[i:i + len(needle)] == needle
+
+def always_true(): return True
+def always_false(): return False
+
+def lib_processutil_isSupported(vm, args):
+  t = TODO("this")
+  return buildBoolean(vm[13], t)
+
+def lib_processutil_launchProcess(vm, args):
+  TODO("this")
+  return vm[14]
+
+def lib_processutil_readBridge(vm, args):
+  type = args[2][1]
+  outputValues = []
+  if (type == 1):
+    outputInt = TODO("this")
+  else:
+    output = []
+    TODO("this")
+  return vm[14]
