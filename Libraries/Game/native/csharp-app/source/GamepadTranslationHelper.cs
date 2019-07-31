dec58951a973a1b47e5da2c8c7eeb35e777de4ec
Blake O'Hare <mspaintmaestro@gmail.com>
2019-07-31 17:36:02 +0900

    Update OpenTK to the latest version (#264)
    Use the latest OpenTK version. This should resolve some recent problems on Mac.

--- Libraries/Game/native/csharp-app/source/GamepadTranslationHelper.cs
+++ Libraries/Game/native/csharp-app/source/GamepadTranslationHelper.cs
@@ -1,93 +1,203 @@
 ﻿using OpenTK.Input;
-using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Interpreter.Libraries.Game
 {
-	public static class GamepadTranslationHelper
-	{
-		private static bool isInitialized = false;
-		private static JoystickDevice[] joysticks = null;
+    public static class GamepadTranslationHelper
+    {
+        private class CrayonGamepad
+        {
+            private enum ButtonTypes
+            {
+                A, B, X, Y,
+                LEFT_SHOULDER, RIGHT_SHOULDER,
+                LEFT_STICK, RIGHT_STICK,
+                BACK,
+                BIG_BUTTON,
+                START,
+            }
 
-		private static void InitializeJoysticks()
-		{
-			if (!isInitialized)
-			{
-				joysticks = GameWindow.Instance.Joysticks.ToArray();
-			}
-		}
+            private enum AxisTypes
+            {
+                LEFT_THUMB_X,
+                LEFT_THUMB_Y,
+                RIGHT_THUMB_X,
+                RIGHT_THUMB_Y,
+                LEFT_TRIGGER,
+                RIGHT_TRIGGER,
+            }
 
-		public static void Initialize(object nativeDevice)
-		{
-			// no initialization necessary
-		}
-		
-		public static void Poll()
-		{
+            private GamePadState state;
+            private ButtonTypes[] buttonNames;
+            private AxisTypes[] axisNames;
+
+            public CrayonGamepad(int index)
+            {
+                this.state = GamePad.GetState(index);
+                GamePadCapabilities cpbl = GamePad.GetCapabilities(index);
+                this.Name = GamePad.GetName(index);
+                List<ButtonTypes> buttons = new List<ButtonTypes>();
+                GamePadButtons btns = this.state.Buttons;
+                if (cpbl.HasAButton) buttons.Add(ButtonTypes.A);
+                if (cpbl.HasBButton) buttons.Add(ButtonTypes.B);
+                if (cpbl.HasXButton) buttons.Add(ButtonTypes.X);
+                if (cpbl.HasYButton) buttons.Add(ButtonTypes.Y);
+                if (cpbl.HasLeftShoulderButton) buttons.Add(ButtonTypes.LEFT_SHOULDER);
+                if (cpbl.HasRightShoulderButton) buttons.Add(ButtonTypes.RIGHT_SHOULDER);
+                if (cpbl.HasLeftStickButton) buttons.Add(ButtonTypes.LEFT_STICK);
+                if (cpbl.HasRightStickButton) buttons.Add(ButtonTypes.RIGHT_STICK);
+                if (cpbl.HasBackButton) buttons.Add(ButtonTypes.BACK);
+                if (cpbl.HasBigButton) buttons.Add(ButtonTypes.BIG_BUTTON);
+                if (cpbl.HasStartButton) buttons.Add(ButtonTypes.START);
+                this.buttonNames = buttons.ToArray();
+
+                this.DPadCount = cpbl.HasDPadDownButton ? 1 : 0;
+
+                List<AxisTypes> axes = new List<AxisTypes>();
+                if (cpbl.HasLeftXThumbStick) { axes.Add(AxisTypes.LEFT_THUMB_X); axes.Add(AxisTypes.LEFT_THUMB_Y); }
+                if (cpbl.HasRightXThumbStick) { axes.Add(AxisTypes.RIGHT_THUMB_X); axes.Add(AxisTypes.RIGHT_THUMB_Y); }
+                if (cpbl.HasLeftTrigger) axes.Add(AxisTypes.LEFT_TRIGGER);
+                if (cpbl.HasRightTrigger) axes.Add(AxisTypes.RIGHT_TRIGGER);
+                this.axisNames = axes.ToArray();
+                this.AxisCount = this.axisNames.Length;
+            }
+
+            public string Name { get; private set; }
+
+            public int DPadCount { get; private set; }
+            public int[] GetDPad(int dpadIndex)
+            {
+                if (this.DPadCount != 0) throw new System.IndexOutOfRangeException();
+                GamePadDPad dpad = this.state.DPad;
+                return new int[] {
+                    dpad.Left == ButtonState.Pressed ? -1 : (dpad.Right == ButtonState.Pressed ? 1 : 0),
+                    dpad.Up == ButtonState.Pressed ? -1 : (dpad.Down == ButtonState.Pressed ? 1 : 0),
+                };
+            }
+
+            public int AxisCount { get; private set; }
+            public double GetAxis(int axisIndex)
+            {
+                switch (this.axisNames[axisIndex])
+                {
+                    case AxisTypes.LEFT_THUMB_X: return this.state.ThumbSticks.Left.X;
+                    case AxisTypes.LEFT_THUMB_Y: return this.state.ThumbSticks.Left.Y;
+                    case AxisTypes.RIGHT_THUMB_X: return this.state.ThumbSticks.Right.X;
+                    case AxisTypes.RIGHT_THUMB_Y: return this.state.ThumbSticks.Right.Y;
+                    case AxisTypes.LEFT_TRIGGER: return this.state.Triggers.Left;
+                    case AxisTypes.RIGHT_TRIGGER: return this.state.Triggers.Right;
+                    default: throw new System.NotImplementedException();
+                }
+            }
+
+            public int ButtonCount { get { return this.buttonNames.Length; } }
+            private ButtonState GetButtonState(int btnIndex)
+            {
+                GamePadButtons b = this.state.Buttons;
+                switch (this.buttonNames[btnIndex])
+                {
+                    case ButtonTypes.A: return b.A;
+                    case ButtonTypes.B: return b.B;
+                    case ButtonTypes.BACK: return b.Back;
+                    case ButtonTypes.BIG_BUTTON: return b.BigButton;
+                    case ButtonTypes.LEFT_SHOULDER: return b.LeftShoulder;
+                    case ButtonTypes.LEFT_STICK: return b.LeftStick;
+                    case ButtonTypes.RIGHT_SHOULDER: return b.RightShoulder;
+                    case ButtonTypes.RIGHT_STICK: return b.RightStick;
+                    case ButtonTypes.START: return b.Start;
+                    case ButtonTypes.X: return b.X;
+                    case ButtonTypes.Y: return b.Y;
+                    default: throw new System.NotImplementedException();
+                }
+            }
+
+            public bool GetButton(int index)
+            {
+                return this.GetButtonState(index) == ButtonState.Pressed;
+            }
+        }
+
+        private static bool isInitialized = false;
+        private static CrayonGamepad[] gamepads = null;
+
+        private static void InitializeJoysticks()
+        {
+            if (!isInitialized)
+            {
+                int count = GameWindow.Instance.GetGamepadCount();
+                GamepadTranslationHelper.gamepads = new CrayonGamepad[count];
+                for (int i = 0; i < count; ++i)
+                {
+                    GamepadTranslationHelper.gamepads[i] = new CrayonGamepad(i);
+                }
+            }
+        }
+
+        public static void Initialize(object nativeDevice)
+        {
+            // no initialization necessary
+        }
+
+        public static void Poll()
+        {
+            // TODO: is this necessary anymore?
+            /*
 #pragma warning disable 612,618
-			GameWindow.Instance.InputDriver.Poll();
+            GameWindow.Instance.InputDriver.Poll();
 #pragma warning restore 612,618
-		}
+            //*/
+        }
 
-		public static int GetCurrentDeviceCount()
-		{
-			InitializeJoysticks();
+        public static int GetCurrentDeviceCount()
+        {
+            InitializeJoysticks();
 
-			return joysticks.Length;
-		}
+            return gamepads.Length;
+        }
 
-		public static string GetDeviceName(object nativeDevice)
-		{
-			JoystickDevice jsDevice = (JoystickDevice)nativeDevice;
-			string rawDescription = jsDevice.Description;
-			if (rawDescription == null)
-			{
-				rawDescription = GetDeviceButtonCount(jsDevice) + " Buttons/" + GetDeviceAxis1dCount(jsDevice) + " Axes";
-			}
-			int ignoreThrough = rawDescription.IndexOf('#');
-			if (ignoreThrough != -1)
-			{
-				string newDescription = rawDescription.Substring(ignoreThrough + 2).Trim();
-				if (newDescription.Length != 0)
-				{
-					rawDescription = "Joystick " + newDescription;
-				}
-			}
-			return rawDescription;
-		}
+        public static string GetDeviceName(object nativeDevice)
+        {
+            return ((CrayonGamepad)nativeDevice).Name;
+        }
 
-		public static object GetDeviceReference(int index)
-		{
-			InitializeJoysticks();
+        public static object GetDeviceReference(int index)
+        {
+            InitializeJoysticks();
 
-			return joysticks[index];
-		}
+            return gamepads[index];
+        }
 
-		public static int GetDeviceButtonCount(object nativeDevice)
-		{
-			return ((JoystickDevice)nativeDevice).Button.Count;
-		}
+        public static int GetDeviceButtonCount(object nativeDevice)
+        {
+            return ((CrayonGamepad)nativeDevice).ButtonCount;
+        }
 
-		public static bool GetDeviceButtonState(object nativeDevice, int buttonIndex)
-		{
-			return ((JoystickDevice)nativeDevice).Button[buttonIndex];
-		}
+        public static bool GetDeviceButtonState(object nativeDevice, int buttonIndex)
+        {
+            return ((CrayonGamepad)nativeDevice).GetButton(buttonIndex);
+        }
 
-		public static int GetDeviceAxis1dCount(object nativeDevice)
-		{
-			return ((JoystickDevice)nativeDevice).Axis.Count;
-		}
+        public static int GetDeviceAxis1dCount(object nativeDevice)
+        {
+            return ((CrayonGamepad)nativeDevice).AxisCount;
+        }
 
-		public static double GetDeviceAxis1dState(object nativeDevice, int axisIndex)
-		{
-			return ((JoystickDevice)nativeDevice).Axis[axisIndex];
-		}
+        public static double GetDeviceAxis1dState(object nativeDevice, int axisIndex)
+        {
+            return ((CrayonGamepad)nativeDevice).GetAxis(axisIndex);
+        }
 
-		public static int GetDeviceAxis2dCount(object nativeDevice)
-		{
-			return 0;
-		}
+        public static int GetDeviceAxis2dCount(object nativeDevice)
+        {
+            return ((CrayonGamepad)nativeDevice).DPadCount;
+        }
 
-		public static void GetDeviceAxis2dState(object nativeDevice, int axisIndex, object ignored) { }
-	}
+        public static void GetDeviceAxis2dState(object nativeDevice, int axisIndex, int[] intBuffer)
+        {
+            int[] state = ((CrayonGamepad)nativeDevice).GetDPad(axisIndex);
+            intBuffer[0] = state[0];
+            intBuffer[1] = state[1];
+        }
+    }
 }
