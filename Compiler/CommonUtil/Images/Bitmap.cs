a722a68ac219dff87283bfe8cfd24687c6e5de5f
Blake O'Hare <mspaintmaestro@gmail.com>
2020-07-05 18:05:32 -0700

    Remove conditional code for OSX bitmap handling now that System.Drawing.Common is being used.

--- Compiler/CommonUtil/Images/Bitmap.cs
+++ Compiler/CommonUtil/Images/Bitmap.cs
@@ -17,41 +17,15 @@ namespace CommonUtil.Images
     {
         public void CheesyCleanup()
         {
-#if OSX
-            // There seems to be a bug in Cairo where the reference counting isn't decremented correctly.
-            // However, making manual multiple redundant calls to cairo_surface_destroy will (safely?) decrement
-            // the reference count.
-            // TODO: track references to Bitmap and verify all have had CheesyCleanup called on it when
-            // the program exits. Be sure to set the bitmap field to null so it doesn't act too much like a memory leak.
-            System.IntPtr handle = this.bitmap.Handle;
-            int refCount = (int)this.bitmap.ReferenceCount;
-            this.bitmap.Dispose();
-            refCount -= 1;
-            if (refCount <= 0)
-            {
-                System.Type nativeMethods = typeof(Cairo.Surface).Assembly.GetType("Cairo.NativeMethods");
-                System.Reflection.MethodInfo surfaceDestroyFp = nativeMethods.GetMethod("cairo_surface_destroy", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
-                for (int i = 0; i < refCount; ++i)
-                {
-                    surfaceDestroyFp.Invoke(null, new object[] { handle });
-                }
-            }
-#endif
         }
 
-#if WINDOWS
         private System.Drawing.Bitmap bitmap;
-#elif OSX
-        // TODO: determine what the reasoning behind this readonly was.
-        private readonly Cairo.ImageSurface bitmap;
-#endif
 
         public int Width { get; set; }
         public int Height { get; set; }
 
         public Bitmap(string filepath)
         {
-#if WINDOWS
             this.bitmap = new System.Drawing.Bitmap(filepath.Replace('/', '\\'));
             this.bitmap.SetResolution(96, 96);
             this.Width = this.bitmap.Width;
@@ -66,16 +40,10 @@ namespace CommonUtil.Images
             g.Dispose();
 
             this.bitmap = newBmp;
-#elif OSX
-            this.bitmap = new Cairo.ImageSurface(filepath);
-            this.Width = this.bitmap.Width;
-            this.Height = this.bitmap.Height;
-#endif
         }
 
         public Bitmap(byte[] bytes)
         {
-#if WINDOWS
             using (System.IO.MemoryStream ms = new System.IO.MemoryStream(bytes))
             {
                 this.bitmap = new System.Drawing.Bitmap(ms);
@@ -83,27 +51,14 @@ namespace CommonUtil.Images
             this.bitmap.SetResolution(96, 96);
             this.Width = this.bitmap.Width;
             this.Height = this.bitmap.Height;
-#elif OSX
-            string tempDir = FileUtil.GetTempDirectory();
-            string tempFile = FileUtil.JoinPath(tempDir, "crayon_image_temp.png");
-            FileUtil.WriteFileBytes(tempFile, bytes);
-            this.bitmap = new Cairo.ImageSurface(tempFile);
-            FileUtil.DeleteFile(tempFile);
-            this.Width = this.bitmap.Width;
-            this.Height = this.bitmap.Height;
-#endif
         }
 
         public Bitmap(int width, int height)
         {
             this.Width = width;
             this.Height = height;
-#if WINDOWS
             this.bitmap = new System.Drawing.Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             this.bitmap.SetResolution(96, 96);
-#elif OSX
-            this.bitmap = new Cairo.ImageSurface(Cairo.Format.ARGB32, width, height);
-#endif
         }
 
         public Bitmap CloneToNewSize(int width, int height)
@@ -132,11 +87,7 @@ namespace CommonUtil.Images
             }
             else
             {
-#if WINDOWS
                 this.bitmap.Save(path);
-#elif OSX
-                this.bitmap.WriteToPng(path);
-#endif
             }
         }
 
