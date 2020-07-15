29bcb8db6e2d6222e352db276385391f68cc9965
Blake O'Hare <mspaintmaestro@gmail.com>
2020-07-15 14:49:20 -0700

    Remove java, python, and PHP native implementation support from all libraries.

--- Libraries/Game/native/python-app/source/graphics2d_helper.py
+++
@@ -1,5 +0,0 @@
-
-def libhelper_imageresources_flipTexture_impl(image, flipH, flipV):
-  pg = TranslationHelper_globals['LIB:GAME:pygame']
-  return pg.transform.flip(image, flipH, flipV)
-TranslationHelper_globals['LIB:GAME:g2dflip'] = libhelper_imageresources_flipTexture_impl
--- Libraries/Web/native/python-app/source/launchBrowser.py
+++
@@ -1,3 +0,0 @@
-def library_web_launch_browser(url):
-	import webbrowser
-	webbrowser.open(url, new = 2)
