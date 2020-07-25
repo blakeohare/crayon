d8d633fda7ea9bb3dacdb3529f419ef8fa6814ce
Blake O'Hare <mspaintmaestro@gmail.com>
2020-07-24 19:53:33 -0700

    Add a demo project for new native C interop stuff.

---
+++ Demos/NativeInteropTest/native/GameInternals/GameInternals/GameInternals.cpp
@@ -0,0 +1,23 @@
+﻿// GameInternals.cpp : Defines the entry point for the application.
+//
+
+#include "GameInternals.h"
+#include <stdlib.h>
+
+using namespace std;
+
+int* create_buffer(int size)
+{
+	int* buffer = (int*)malloc(sizeof(int) * (size + 1));
+	buffer[0] = size;
+	return buffer + 1;
+}
+
+int* blarg_test(int argc, int* argv)
+{
+	int* buffer = create_buffer(argc);
+	for (int i = 0; i < argc; ++i) {
+		buffer[i] = argv[i] * 2;
+	}
+	return buffer;
+}
---
+++ Demos/NativeInteropTest/native/GameInternals/GameInternals/GameInternals.h
@@ -0,0 +1,8 @@
+﻿// GameInternals.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+//#include <iostream>
+
+int* blarg_test(int argc, int* argv);
