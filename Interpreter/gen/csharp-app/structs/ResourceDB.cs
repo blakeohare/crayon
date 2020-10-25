1fc7bd05a1bd768bcd3ad6b3982fb5b53dd24e20
Blake O'Hare <mspaintmaestro@gmail.com>
2020-10-25 02:13:37 -0700

    ImageLibrary -> all core functions have their Pastel-based implementation and call into stubbed native functions.

--- Interpreter/gen/csharp-app/structs/ResourceDB.cs
+++ Interpreter/gen/csharp-app/structs/ResourceDB.cs
@@ -9,12 +9,14 @@ namespace Interpreter.Structs
         public Dictionary<string, string[]> filesPerDirectory;
         public Dictionary<string, ResourceInfo> fileInfo;
         public List<Value> dataList;
+        public string imageAtlasManifest;
 
-        public ResourceDB(Dictionary<string, string[]> filesPerDirectory, Dictionary<string, ResourceInfo> fileInfo, List<Value> dataList)
+        public ResourceDB(Dictionary<string, string[]> filesPerDirectory, Dictionary<string, ResourceInfo> fileInfo, List<Value> dataList, string imageAtlasManifest)
         {
             this.filesPerDirectory = filesPerDirectory;
             this.fileInfo = fileInfo;
             this.dataList = dataList;
+            this.imageAtlasManifest = imageAtlasManifest;
         }
     }
 
