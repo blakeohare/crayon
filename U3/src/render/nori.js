51cdc7983b56d2115caa6aa6dee250518d206e10
Blake O'Hare <mspaintmaestro@gmail.com>
2020-11-02 17:05:52 -0800

    [U3]Nori frame now supports adding a keydown/keyup handler.

--- U3/src/render/nori.js
+++ U3/src/render/nori.js
@@ -34,7 +34,102 @@ var ctx = {
 		scroll: 'scroll',
 		crop: 'hidden'
 	},
-	childrenLayoutFnByType: {}
+	childrenLayoutFnByType: {},
+	hasKeyDownListener: false,
+	hasKeyUpListener: false,
+	isKeyPressed: {},
+};
+
+window.addEventListener('keydown', e => {
+	if (ctx.hasKeyDownListener) handleKeyEvent(e, true);
+});
+
+window.addEventListener('keyup', e => {
+	if (ctx.hasKeyUpListener) handleKeyEvent(e, false);
+});
+
+const handleKeyEvent = (e, isDown) => {
+	let keyName = keyEventToType(e);
+	if (isDown && ctx.isKeyPressed[keyName]) return;
+	ctx.isKeyPressed[keyName] = isDown;
+	let arg = keyName + '|' + keyEventToChar(e);
+	if (e.shiftKey) arg += '|s';
+	if (e.ctrlKey) arg += '|c';
+	if (e.altKey) arg += '|a';
+	platformSpecificHandleEvent(-1, 'frame.' + (isDown ? 'keydown' : 'keyup'), arg);
+};
+
+const keyEventToChar = e => {
+	let c = e.key;
+	switch (c) {
+		// These mess with protocol delimiters
+		case ' ': return 'SPACE';
+		case '|': return 'PIPE';
+		case 'Enter': return '\n';
+		case 'Tab': return '\t';
+		
+		default:
+			if (c.length > 1) return '';
+			break;
+	}
+	return c;
+};
+
+const keyEventToType = e => {
+	let c = e.code;
+	switch (c) {
+		case 'ShiftRight':
+		case 'ShiftLeft':
+			return 'shift';
+		case 'ControlLeft':
+		case 'ControlRight':
+			return 'ctrl';
+		case 'AltLeft':
+		case 'AltRight':
+			return 'alt';
+
+		case 'ArrowLeft': return 'left';
+		case 'ArrowRight': return 'right';
+		case 'ArrowUp': return 'up';
+		case 'ArrowDown': return 'down';
+
+		case 'Minus': return '-';
+		case 'Equal': return '=';
+		case 'BracketLeft': return '[';
+		case 'BracketRight': return ']';
+		case 'Backslash': return '\\';
+		case 'Semicolon': return ';';
+		case 'Quote': return "'";
+		case 'Comma': return ',';
+		case 'Period': return '.';
+		case 'Slash': return '/';
+		case 'Backquote': return '`';
+
+		case 'ContextMenu': return 'context';
+		case 'MetaLeft': return 'os';
+
+		case 'Backspace':
+		case 'CapsLock':
+		case 'Enter':
+		case 'Tab':
+		case 'Space': 
+		case 'Escape':
+		case 'F10':
+		case 'F11':
+		case 'F12':
+		case 'Home':
+		case 'End':
+		case 'PageUp':
+		case 'PageDown':
+		case 'Delete':
+		case 'Insert':
+		case 'PrintScreen':
+			return c.toLowerCase();
+	}
+	if (c.startsWith('Key') && c.length == 4) return c.charAt(3).toLowerCase();
+	if (c.startsWith('Digit') && c.length == 6) return c.charAt(5);
+	if (c.length == 2 && c.charAt(0) == 'F' && parseInt(c.charAt(1)) + '' == c.charAt(1)) return c.toLowerCase();
+	return 'unknown:' + c;
 };
 
 function setFrameRoot(root) {
@@ -443,6 +538,19 @@ function flushUpdates(data) {
 
 			// noop
 			case 'NO': break;
+
+			case 'FA': // Frame attribute
+				propertyKey = items[i++];
+				propertyValue = items[i++];
+				switch (propertyKey) {
+					case 'frame.keydown':
+						ctx.hasKeyDownListener = true;
+						break;
+					case 'frame.keyup':
+						ctx.hasKeyUpListener = true;
+						break;
+				}
+				break;
 			
 			case 'PF': // property full state
 				type = items[i++];
