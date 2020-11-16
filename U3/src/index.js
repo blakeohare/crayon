a4ce9e29aa3ae1285c079a1446e54bb9bfc9eeda
Blake O'Hare <mspaintmaestro@gmail.com>
2020-11-15 21:55:45 -0800

    Exported C# projects now launch the installed (%CRAYON_HOME%) version of U3 at runtime without having to launch U3 manually.
    This is not ideal, but unblocks progress.

--- U3/src/index.js
+++ U3/src/index.js
@@ -2,11 +2,29 @@ const { app } = require('electron');
 const { createHub } = require('./messagehubclient');
 const renderwindow = require('./renderwindow.js');
 
+let args = (() => {
+    let output = {};
+    let argv = process.argv;
+    for (let i = 0; i < argv.length; ++i) {
+        let arg = argv[i];
+        if (arg.startsWith('--u3:')) {
+            let key = arg.substr('--u3:'.length);
+            let value = i + 1 < argv.length ? argv[i + 1] : '';
+            output[key] = value;
+        }
+    }
+    return output;
+})();
+
+let u3Token = args.token || 'u3debug';
+let pid = args.pid === undefined ? null : args.pid;
+if (pid <= 0) pid = null; // TODO: monitor this process ID and terminate the u3 window if the pid goes away. 
+
 app.whenReady().then(() => {
 
     let rwindow = null;
 
-    const hub = createHub('u3debug');
+    const hub = createHub(u3Token);
 
     hub.addListener('u3init', (msg, cb) => {
         let { title, width, height, initialData } = msg;
