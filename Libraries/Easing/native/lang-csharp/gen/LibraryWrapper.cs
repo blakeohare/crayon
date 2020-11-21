7b5c7f66bf94f2ba9992242f473310f2a450621a
Blake O'Hare <mspaintmaestro@gmail.com>
2020-11-20 23:11:35 -0800

    generated code churn caused by fixing a C# styling error in Pastel.

--- Libraries/Easing/native/lang-csharp/gen/LibraryWrapper.cs
+++ Libraries/Easing/native/lang-csharp/gen/LibraryWrapper.cs
@@ -8,7 +8,8 @@ namespace Interpreter.Libraries.Easing
     {
         private static Dictionary<string, System.Func<object[], object>> PST_ExtCallbacks = new Dictionary<string, System.Func<object[], object>>();
 
-        public static void PST_RegisterExtensibleCallback(string name, System.Func<object[], object> func) {
+        public static void PST_RegisterExtensibleCallback(string name, System.Func<object[], object> func)
+        {
             PST_ExtCallbacks[name] = func;
         }
 
--- Libraries/Zip/native/csharp-app/gen/LibraryWrapper.cs
+++ Libraries/Zip/native/csharp-app/gen/LibraryWrapper.cs
@@ -10,7 +10,8 @@ namespace Interpreter.Libraries.Zip
 
         private static Dictionary<string, System.Func<object[], object>> PST_ExtCallbacks = new Dictionary<string, System.Func<object[], object>>();
 
-        public static void PST_RegisterExtensibleCallback(string name, System.Func<object[], object> func) {
+        public static void PST_RegisterExtensibleCallback(string name, System.Func<object[], object> func)
+        {
             PST_ExtCallbacks[name] = func;
         }
 
