95eb97f93ae8dd9a0bc56b5f055f58a3b1ba877f
Blake O'Hare <mspaintmaestro@gmail.com>
2020-12-03 17:00:51 -0800

    U3: There seems to be a problem with the files written by the temp library not getting recognized. Picking my own file names randomly instead seems to fix the issue, so possibly not an IconEncoder problem.

--- U3/src/util.js
+++ U3/src/util.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const temp = require('temp');
+const os = require('os');
+const path = require('path');
 
 let base64ToText = b64Value => {
     return Buffer.from(b64Value, 'base64').toString('utf8');
@@ -9,18 +11,27 @@ let textToBase64 = rawValue => {
     return Buffer.from(rawValue).toString('base64');
 };
 
+let charsForGibberish = 'abcdefghijklmnopqrstuvwxyz0123456789';
+let generateGibberish = len => {
+    let sb = [];
+    while (len --> 0) {
+        sb.push(charsForGibberish.charAt(Math.floor(Math.random() * charsForGibberish.length)));
+    }
+    return sb.join('');
+};
+
 let writeBase64ToTempFile = (prefix, suffix, data) => {
     let resolve = null;
     let p = new Promise(res => {
         resolve = res;
     });
-    temp.open({ prefix, suffix }, (err, info) => {
-        let buf = Buffer.from(data, 'base64');
-        console.log(info.fd);
-        fs.write(info.fd, buf, (err) => {
-            resolve({
-                path: info.path,
-            });
+    
+    let buf = Buffer.from(data, 'base64');
+    let iconpath = path.join(os.tmpdir(), prefix + generateGibberish(20) + suffix);
+    
+    fs.writeFile(iconpath, buf, (err) => {
+        resolve({
+            path: iconpath,
         });
     });
     return p;
@@ -30,4 +41,5 @@ module.exports = {
     base64ToText,
     textToBase64,
     writeBase64ToTempFile,
+    generateGibberish,
 };
