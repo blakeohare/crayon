4948e3e9c0bc742b2ee158234d91111002eb7742
Blake O'Hare <mspaintmaestro@gmail.com>
2020-12-05 14:48:02 -0800

    U3: Electron based U3 now honors the keepAspectRatio flag

--- U3/src/render/shim.js
+++ U3/src/render/shim.js
@@ -27,18 +27,62 @@ function flushMessageQueue() {
 	}
 }
 
-function getWindowSize() {
-	let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-	let height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-	return [width, height];
-}
+let getWindowSize = () => {
+	return [window.innerWidth, window.innerHeight];
+};
 
 function shimInit(uiData, options) {
-	if (options && options.keepAspectRatio) {
-		// window.alert("Options are telling me to keep the aspect ratio");
-	}
-	let noriRoot = document.getElementById('html_render_host');
 	let shownCb = () => { window.sendMessage('shown', true); };
 	let eventBatchSender = data => { window.sendMessage('eventBatch', data); };
+	let keepAspectRatio = options && options.keepAspectRatio;
+	let targetAspectRatio = window.innerWidth / Math.max(1, window.innerHeight);
+	let noriRoot;
+	let root = document.getElementById('html_render_host');
+	if (keepAspectRatio) {
+		let currentSize = [window.innerWidth, window.innerHeight];
+		getWindowSize = () => currentSize.slice(0);
+		let inner = document.createElement('div');
+		let outStyle = root.style;
+		let inStyle = inner.style;
+		outStyle.position = 'relative';
+		outStyle.backgroundColor = '#000000';
+		outStyle.overflow = 'hidden';
+		inStyle.position = 'absolute';
+		inStyle.overflow = 'hidden';
+		root.append(inner);
+		noriRoot = inner;
+		let setSize = () => {
+			let width = window.innerWidth;
+			let height = window.innerHeight;
+			let aspectRatio = width / Math.max(1, height);
+			let innerWidth;
+			let innerHeight;
+			let left = 0;
+			let top = 0;
+			if (targetAspectRatio > aspectRatio) {
+				innerWidth = width;
+				innerHeight = Math.max(1, Math.floor(width / targetAspectRatio));
+				top = Math.floor((height - innerHeight) / 2); 
+			} else {
+				innerHeight = height;
+				innerWidth = Math.max(1, Math.floor(height * targetAspectRatio));
+				left = Math.floor((width - innerWidth) / 2);
+			}
+			outStyle.width = width + 'px';
+			outStyle.height = height + 'px';
+			inStyle.width = innerWidth + 'px';
+			inStyle.height = innerHeight + 'px';
+			inStyle.left = left + 'px';
+			inStyle.top = top + 'px';
+			currentSize = [innerWidth, innerHeight];
+		};
+		window.addEventListener('resize', () => { 
+			setSize(); 
+			NoriLayout.doLayoutPass();
+		});
+		setSize();
+	} else {
+		noriRoot = root;
+	}
 	noriInit(noriRoot, uiData, shownCb, eventBatchSender);
 }
