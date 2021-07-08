341f1184b16d8b52802d9880522bcdf362c75597
Blake O'Hare <mspaintmaestro@gmail.com>
2021-07-07 18:05:03 -0700

    Split CompilationBundle into InternalCompilationBundle and ExternalCompilationBundle to prepare the compiler for WaxService conversion.

--- Compiler/Parser/CompilationBundle.cs
+++ Compiler/Parser/CompilationBundle.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace Parser
 {
-    public class CompilationBundle
+    public class InternalCompilationBundle
     {
         public string ByteCode { get; set; }
         internal CompilationScope RootScope { get; set; }
@@ -12,5 +12,29 @@ namespace Parser
         public ExternalAssemblyMetadata[] AllScopesMetadata { get { return this.AllScopes.Select(s => s.Metadata).ToArray(); } }
         public Common.Error[] Errors { get; set; }
         public bool HasErrors { get { return this.Errors != null && this.Errors.Length > 0; } }
+    }
+
+    public class ExternalCompilationBundle
+    {
+        public string ByteCode { get; set; }
+        public string DependencyTreeJson { get; set; }
+        public bool UsesU3 { get; set; }
+        public Common.Error[] Errors { get; set; }
+        public bool HasErrors { get { return this.Errors != null && this.Errors.Length > 0; } }
+    }
+
+    public static class CompilerTODO
+    {
+        public static ExternalCompilationBundle Convert(InternalCompilationBundle icb)
+        {
+            string depTree = AssemblyDependencyUtil.GetDependencyTreeJson(icb.RootScopeDependencyMetadata).Trim();
+            return new ExternalCompilationBundle()
+            {
+                DependencyTreeJson = depTree,
+                ByteCode = icb.ByteCode,
+                UsesU3 = icb.AllScopesMetadata.Any(a => a.ID == "U3Direct"),
+                Errors = icb.Errors,
+            };
+        }
     }
 }
