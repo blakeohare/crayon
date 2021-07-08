23ceecc365597ae8e32cc7f830491125130bc852
Blake O'Hare <mspaintmaestro@gmail.com>
2021-07-08 13:12:59 -0700

    Stub out a service for Extensions.

---
+++ Compiler/Extensions/ExtensionsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public class ExtensionsService : CommonUtil.Wax.WaxService
+    {
+        public ExtensionsService() : base("extensions") { }
+
+        public override void HandleRequest(Dictionary<string, object> request, Func<Dictionary<string, object>, bool> cb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
