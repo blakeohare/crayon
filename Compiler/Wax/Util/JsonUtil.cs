a388730f0ce15c42b8ab8b91356a15ee75077c18
Blake O'Hare <mspaintmaestro@gmail.com>
2021-11-30 23:39:44 -0800

    Properly pass arguments to the runtime service from the cbx extension service. Prefer building extensions from the .build file instead of using pre-existing cbx files, if source code is present.
    Fix typo in FileIO.

---
+++ Compiler/Wax/Util/JsonUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wax.Util
+{
+    public static class JsonUtil
+    {
+        public static string SerializeJson(IDictionary<string, object> root)
+        {
+            return new JsonBasedObject(root).ToJson();
+        }
+    }
+}
