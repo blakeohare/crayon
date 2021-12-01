51db65d2602d72f0f0d31bf24de02dbc4b448856
Blake O'Hare <mspaintmaestro@gmail.com>
2021-11-30 16:30:28 -0800

    Move ConsoleWriter into the RouterService.

---
+++ Compiler/Router/ConsoleWriter.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace Router
+{
+    public enum ConsoleMessageType
+    {
+        BUILD_WARNING,
+        COMPILER_INFORMATION,
+        DEFAULT_PROJ_EXPORT_INFO,
+        GENERAL_COMPILATION_ERROR,
+        LIBRARY_TREE,
+        PARSER_ERROR,
+        REMOTE_ASSEMBLY_ERROR,
+        STATUS_CHANGE,
+        USAGE_NOTES,
+    }
+
+    public static class ConsoleWriter
+    {
+        private static bool prefixesEnabled = false;
+
+        public static void Print(ConsoleMessageType messageType, string message)
+        {
+            if (messageType == ConsoleMessageType.STATUS_CHANGE && !prefixesEnabled)
+            {
+                return;
+            }
+
+            string prefix = prefixesEnabled ? (messageType.ToString() + ":") : "";
+            foreach (string line in message.Split('\n').Select(s => s.TrimEnd()))
+            {
+                CommonUtil.Print.Line(prefix + line);
+            }
+
+            return;
+        }
+
+        public static void EnablePrefixes()
+        {
+            prefixesEnabled = true;
+        }
+    }
+}
