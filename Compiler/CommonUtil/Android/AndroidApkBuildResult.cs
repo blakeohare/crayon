4c19103bb0f79b6735ff5a1a3a3f4753975a7893
Blake O'Hare <mspaintmaestro@gmail.com>
2021-11-30 23:40:36 -0800

    Delete the C# AndroidUtil since this is now a CBX extension.

--- Compiler/CommonUtil/Android/AndroidApkBuildResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CommonUtil.Android
-{
-    public class AndroidApkBuildResult
-    {
-        public string ApkPath { get; set; }
-        public string Error { get; set; }
-        public bool HasError { get; set; }
-    }
-}
