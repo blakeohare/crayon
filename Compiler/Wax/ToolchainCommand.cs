8457a516fbb0335034ca488182e4c41b999636eb
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-05 18:14:51 -0800

    Separate some of the toolchain args into build args.
    These will be all the attributes that can be defined in a build file and overridden by the command line. The eventual goal is that build file parsing will simply create a list of BuildArgs and ExtensionArgs.

--- Compiler/Wax/ToolchainCommand.cs
+++ Compiler/Wax/ToolchainCommand.cs
@@ -13,6 +13,7 @@ namespace Wax
         public string[] RuntimeArgs { get { return this.GetStrings("runtimeArgs"); } set { this.SetStrings("runtimeArgs", value); } }
         public ExtensionArg[] ExtensionArgs { get { return this.GetObjectsAsType<ExtensionArg>("extensions"); } set { this.SetObjects("extensions", value); } }
         public ToolchainArg[] ToolchainArgs { get { return this.GetObjectsAsType<ToolchainArg>("toolchainArgs"); } set { this.SetObjects("toolchainArgs", value); this.argCache = null; } }
+        public BuildArg[] BuildArgs { get { return this.GetObjectsAsType<BuildArg>("buildArgs"); } set { this.SetObjects("buildArgs", value); } }
         public string BuildFile { get { return this.GetString("buildFile"); } set { this.SetString("buildFile", value); } }
         public string CbxFile { get { return this.GetString("cbxFile"); } set { this.SetString("cbxFile", value); } }
 
@@ -42,15 +43,24 @@ namespace Wax
             return this.argCache.ContainsKey(name) ? this.argCache[name] : null;
         }
 
+        private string GetBuildArg(string name)
+        {
+            foreach (BuildArg ba in this.BuildArgs ?? new BuildArg[0])
+            {
+                if (ba.Name == name) return ba.Value;
+            }
+            return null;
+        }
+
         public bool UseJsonOutput { get { return this.GetToolchainArg("useJsonOutput") != null; } }
-        public bool UseOutputPrefixes {  get { return this.GetToolchainArg("useOutputPrefixes") != null; } }
+        public bool UseOutputPrefixes { get { return this.GetToolchainArg("useOutputPrefixes") != null; } }
         public bool ShowLibraryStackTraces { get { return this.GetToolchainArg("showLibStack") != null; } }
 
-        // These need to be converted into build args
-        public bool IsErrorCheckOnly { get { return this.GetToolchainArg("buildErrorCheck") != null; } }
-        public string BuildTarget { get { return this.GetToolchainArg("target"); } }
-        public string OutputDirectoryOverride { get { return this.GetToolchainArg("outputDirOverride"); } }
-        public string CbxExportPath { get { return this.GetToolchainArg("cbxExportPath"); } }
+        // These build args are used by the Router service.
+        public bool IsErrorCheckOnly { get { return this.GetBuildArg("buildErrorCheck") != null; } }
+        public string BuildTarget { get { return this.GetBuildArg("target"); } }
+        public string OutputDirectoryOverride { get { return this.GetBuildArg("outputDirOverride"); } }
+        public string CbxExportPath { get { return this.GetBuildArg("cbxExportPath"); } }
     }
 
     public class ToolchainArg : JsonBasedObject
@@ -68,6 +78,15 @@ namespace Wax
         public ExtensionArg(IDictionary<string, object> data) : base(data) { }
 
         public string Extension { get { return this.GetString("ext"); } set { this.SetString("ext", value); } }
+        public string Name { get { return this.GetString("name"); } set { this.SetString("name", value); } }
+        public string Value { get { return this.GetString("value"); } set { this.SetString("value", Value); } }
+    }
+
+    public class BuildArg : JsonBasedObject
+    {
+        public BuildArg() : base() { }
+        public BuildArg(IDictionary<string, object> data) : base(data) { }
+
         public string Name { get { return this.GetString("name"); } set { this.SetString("name", value); } }
         public string Value { get { return this.GetString("value"); } set { this.SetString("value", Value); } }
     }
