c6911cf9f9de0dd5a4ad659f1c8ab60b812fc9f2
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-08 21:16:25 -0800

    Add implementation to U3Service that handles all the request types (at least in a stubbed fashion) and make the U3Direct library use the wax-based service.

---
+++ Compiler/U3Windows/U3Window.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace U3Windows
+{
+    internal class U3Window
+    {
+        private static int idAlloc = 1;
+
+        public int ID { get; private set; }
+        public Func<Dictionary<string, object>, bool> EventsListener { get; set; }
+        public Func<Dictionary<string, object>, bool> BatchListener { get; set; }
+        public Func<Dictionary<string, object>, bool> ClosedListener { get; set; }
+        public Func<Dictionary<string, object>, bool> LoadedListener { get; set; }
+
+        public U3Window()
+        {
+            this.ID = idAlloc++;
+        }
+
+        public void Show(string title, int width, int height, string icon, bool keepAspectRatio, object[] initialData, Func<bool> shownAndReadyCallback)
+        {
+
+        }
+    }
+}
