7abe24f08178754ab1eeb9bc9ae1cc15f09ee956
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-08 17:06:33 -0800

    Add a stubbed-out U3 Service.

---
+++ Compiler/U3Windows/U3Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Wax;
+
+namespace U3Windows
+{
+    public class U3Service : WaxService
+    {
+        public U3Service() : base("u3") { }
+
+        public override Task<Dictionary<string, object>> HandleRequest(Dictionary<string, object> request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
