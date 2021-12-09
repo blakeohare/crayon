418b9c14a54d7b41a4b3bec601248ba8a89b1af4
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-08 17:06:33 -0800

    Add a stubbed-out U3 Service.

--- Compiler/Crayon/Program.cs
+++ Compiler/Crayon/Program.cs
@@ -42,6 +42,7 @@ namespace Crayon
             waxHub.RegisterService(new AssemblyResolver.AssemblyService());
             waxHub.RegisterService(new Runtime.RuntimeService());
             waxHub.RegisterService(new Builder.BuilderService());
+            waxHub.RegisterService(new U3Windows.U3Service());
 
             Wax.ToolchainCommand command = FlagParser.Parse(args);
 
