d37cec22cdbebd2f6d16ba0c4d56a2fbb76e534e
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-09 15:48:55 -0800

    Dynamically read JavaScript resources for the U3Window instead of the hardcoded test string.
    The JavaScript is still a test string, but it's coming from resource files now and is being wrapped in <script> tags.

--- Compiler/U3Windows/U3Window.cs
+++ Compiler/U3Windows/U3Window.cs
@@ -42,7 +42,8 @@ namespace U3Windows
         private async void LoadedHandler(object sender, System.Windows.RoutedEventArgs e)
         {
             await this.webview.EnsureCoreWebView2Async();
-            this.webview.NavigateToString("<html><body style=\"background-color: #808;color:#fff;\">Hello, World</body></html>");
+
+            this.webview.NavigateToString(GetU3Source());
 
             // TODO: the JavaScript itself needs to send a loaded notification back to C# instead of this 1 second wait.
             await Task.Delay(1000);
@@ -57,5 +58,32 @@ namespace U3Windows
             this.eventLoopTicklerTimer.Start();
         }
 
+        private static Wax.EmbeddedResourceReader resourceReader = new Wax.EmbeddedResourceReader(typeof(U3Window).Assembly);
+
+        private static string GetU3Source()
+        {
+            List<string> output = new List<string>();
+
+            output.Add(string.Join('\n',
+                "<!DOCTYPE html>\n",
+                "<html>",
+                "<head>\n"));
+
+            string[] files = resourceReader.ListFiles("u3");
+
+            foreach (string file in files)
+            {
+                string content = "\n<script>\n// " + file + ".js\n" + resourceReader.GetText(file) + "\n</script>\n";
+                output.Add(content);
+            }
+
+            output.Add(string.Join('\n',
+                "</head>",
+                "<body>",
+                "</body>",
+                "</html>"));
+
+            return string.Join("", output);
+        }
     }
 }
