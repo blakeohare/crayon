09fd11ff6b8fe14df9405f07a0cf362beeb9aec5
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-10 14:02:57 -0800

    U3Window now loads the U3 content and sends some messages back and forth (although currently blocked by event loop)

---
+++ Compiler/U3Windows/JsBridge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace U3Windows
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    public class JsBridge
+    {
+        private Func<string, string, string> vmBoundCallback;
+        public JsBridge(Func<string, string, string> vmBoundCallback)
+        {
+            this.vmBoundCallback = vmBoundCallback;
+        }
+
+        public string sendToCSharp(string msgType, string msgPayloadJson)
+        {
+            this.vmBoundCallback(msgType, msgPayloadJson);
+            return "{}";
+        }
+    }
+}
--- Compiler/U3Windows/u3/test.js
+++
@@ -1,5 +0,0 @@
-﻿window.addEventListener('load', () => {
-	window.alert('ahoy!');
-	document.write('<h1>Ahoy</h1>');
-
-});
