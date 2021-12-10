600899149a5c08dff3538a1908e1dc05cc75b170
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-10 13:55:02 -0800

    Add the U3 files to the U3Windows project.

---
+++ Compiler/U3Windows/u3/nori_gamepad.js
@@ -0,0 +1,82 @@
+﻿const NoriGamepad = (() => {
+
+    let gamepads = [];
+    let isInitialized = false;
+
+    window.addEventListener('gamepadconnected', e => {
+        let index = gamepads.length;
+        window.blarg = e.gamepad;
+        gamepads.push({
+            name: e.gamepad.id,
+            nativeIndex: e.gamepad.index,
+            reported: false,
+            buttons: {},
+            buttonCount: e.gamepad.buttons.length,
+            axes: {},
+            axisCount: e.gamepad.axes.length,
+        });
+        if (isInitialized) {
+            reportNewDevice(index);
+        }
+    });
+
+    let init = () => {
+        isInitialized = true;
+        for (let i = 0; i < gamepads.length; ++i) {
+            reportNewDevice(i);
+        }
+    };
+
+    let pollDevices = () => {
+        let changes = ['state'];
+        let nativeGamepads = navigator.getGamepads();
+        for (let gpIndex = 0; gpIndex < gamepads.length; gpIndex++) {
+            let gp = gamepads[gpIndex];
+            let ngp = nativeGamepads[gp.nativeIndex];
+            let axes = ngp.axes;
+            let buttons = ngp.buttons;
+
+            for (let i = 0; i < axes.length; ++i) {
+                let value = Math.floor(axes[i] * 1000);
+                if (gp.axes[i] !== value) {
+                    gp.axes[i] = value;
+                    changes.push(gpIndex + '|A|' + i + '|' + value);
+                }
+            }
+
+            for (let i = 0; i < buttons.length; ++i) {
+                let value = !!buttons[i].pressed;
+                if (gp.buttons[i] !== value) {
+                    gp.buttons[i] = value;
+                    changes.push(gpIndex + '|B|' + i + '|' + (value ? '1' : '0'));
+                }
+            }
+        }
+        if (changes.length > 1) {
+            platformSpecificHandleEvent(-1, 'gamepad', changes.join(':'));
+        }
+    };
+
+    let reportNewDevice = index => {
+        let gp = gamepads[index];
+        let arg = [
+            'device',
+            index,
+            NoriUtil.encodeB64(gp.name),
+            gp.axisCount,
+            gp.buttonCount,
+        ].join(':');
+        platformSpecificHandleEvent(-1, 'gamepad', arg);
+        if (index === 0) {
+            setTimeout(() => {
+                setInterval(() => {
+                    pollDevices();
+                }, 15);
+            }, 50);
+        }
+    };
+
+    return {
+        init,
+    };
+})();
