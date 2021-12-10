387ffcd2894f3e9b427cf7f643aa9173f438b523
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-10 14:02:57 -0800

    U3Window now loads the U3 content and sends some messages back and forth (although currently blocked by event loop)

--- Compiler/U3Windows/U3Window.cs
+++ Compiler/U3Windows/U3Window.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace U3Windows
@@ -26,64 +27,127 @@ namespace U3Windows
             this.ID = idAlloc++;
         }
 
-        public void Show(string title, int width, int height, string icon, bool keepAspectRatio, object[] initialData, int vmId, Func<bool> closedCallback)
+        public async Task Show(string title, int width, int height, string icon, bool keepAspectRatio, object[] initialData, int vmId, Func<bool> closedCallback)
         {
             this.nativeWindow = new System.Windows.Window() { Title = title, Width = width, Height = height };
             this.webview = new Microsoft.Web.WebView2.Wpf.WebView2();
             this.nativeWindow.Content = this.webview;
 
-            this.nativeWindow.Loaded += LoadedHandler;
+            this.nativeWindow.Loaded += (sender, e) => { LoadedHandler(initialData, keepAspectRatio); };
 
+            await Task.Delay(1);
             this.nativeWindow.ShowDialog();
 
             this.eventLoopTicklerTimer.Stop();
         }
 
-        private async void LoadedHandler(object sender, System.Windows.RoutedEventArgs e)
+        private void SendToJavaScript(Dictionary<string, object> data)
         {
-            await this.webview.EnsureCoreWebView2Async();
+            string js = "window.csharpToJavaScript(" + Wax.Util.JsonUtil.SerializeStringRoot(Wax.Util.JsonUtil.SerializeJson(data)) + ");";
+            this.webview.ExecuteScriptAsync(js);
+        }
 
-            this.webview.NavigateToString(GetU3Source());
-
-            // TODO: the JavaScript itself needs to send a loaded notification back to C# instead of this 1 second wait.
-            await Task.Delay(1000);
-
-            this.LoadedListener(new Dictionary<string, object>() { { "shown", true } }); // queues up the unblock execution lock
-
+        private async void LoadedHandler(object[] initialData, bool keepAspectRatio)
+        {
             this.eventLoopTicklerTimer = new System.Windows.Threading.DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(1) };
             this.eventLoopTicklerTimer.Tick += (sender, e) =>
             {
                 this.EventLoopTickler();
             };
-            this.eventLoopTicklerTimer.Start();
+
+            await this.webview.EnsureCoreWebView2Async();
+
+            List<Dictionary<string, object>> queueEventsMessages = new List<Dictionary<string, object>>();
+
+            this.webview.CoreWebView2.AddHostObjectToScript("u3bridge", new JsBridge((type, payloadJson) =>
+            {
+                IDictionary<string, object> wrappedData = new Wax.Util.JsonParser("{\"rawData\": " + payloadJson + "}").ParseAsDictionary();
+                object rawData = wrappedData["rawData"];
+                int bridgeThreadId1 = System.Threading.Thread.CurrentThread.ManagedThreadId;
+                switch (type)
+                {
+                    case "bridgeReady":
+                        this.SendToJavaScript(new Dictionary<string, object>()
+                        {
+                            { "buffer", initialData },
+                            { "options", new Dictionary<string, object>()
+                                {
+                                    { "keepAspectRatio", keepAspectRatio }
+                                }
+                            }
+                        });
+                        break;
+                    case "shown":
+                        this.LoadedListener(new Dictionary<string, object>());
+                        this.eventLoopTicklerTimer.Start();
+                        if (queueEventsMessages != null)
+                        {
+                            foreach (Dictionary<string, object> evMsg in queueEventsMessages)
+                            {
+                                this.EventsListener(evMsg);
+                            }
+                            queueEventsMessages = null;
+                        }
+                        break;
+
+                    case "events":
+                        Dictionary<string, object> eventData = new Dictionary<string, object>() {
+                            { "msgs", rawData },
+                        };
+                        if (queueEventsMessages != null)
+                        {
+                            queueEventsMessages.Add(eventData);
+                        }
+                        else
+                        {
+                            this.EventsListener(eventData);
+                        }
+                        break;
+
+                    default:
+                        throw new NotImplementedException();
+                }
+                return "{}";
+            }));
+
+#if DEBUG
+            string tempDir = System.Environment.GetEnvironmentVariable("TEMP");
+            string debugHtmlFile = System.IO.Path.Combine(tempDir, "u3-debug.html");
+            System.IO.File.WriteAllText(debugHtmlFile, GetU3Source());
+            this.webview.CoreWebView2.Navigate("file:///" + debugHtmlFile.Replace('\\', '/'));
+#else
+            this.webview.NavigateToString(GetU3Source());
+#endif
         }
 
         private static Wax.EmbeddedResourceReader resourceReader = new Wax.EmbeddedResourceReader(typeof(U3Window).Assembly);
 
         private static string GetU3Source()
         {
-            List<string> output = new List<string>();
-
-            output.Add(string.Join('\n',
-                "<!DOCTYPE html>\n",
-                "<html>",
-                "<head>\n"));
-
-            string[] files = resourceReader.ListFiles("u3");
-
-            foreach (string file in files)
+            string[] lines = resourceReader.GetText("u3/index.html", "\n").Split('\n');
+            List<string> newLines = new List<string>();
+            foreach (string line in lines)
             {
-                string content = "\n<script>\n// " + file + ".js\n" + resourceReader.GetText(file) + "\n</script>\n";
-                output.Add(content);
+                if (line.Contains("SCRIPTS_GO_HERE"))
+                {
+                    string[] files = resourceReader.ListFiles("u3/").Where(name => name.EndsWith(".js")).ToArray();
+
+                    foreach (string file in files)
+                    {
+                        newLines.Add("<script>");
+                        newLines.Add("// " + file);
+                        newLines.Add(resourceReader.GetText(file, "\n").Trim());
+                        newLines.Add("</script>");
+                    }
+                }
+                else
+                {
+                    newLines.Add(line);
+                }
             }
 
-            output.Add(string.Join('\n',
-                "</head>",
-                "<body>",
-                "</body>",
-                "</html>"));
-
-            return string.Join("", output);
+            string u3Source = string.Join("\n", newLines);
+            return u3Source;
         }
     }
 }
