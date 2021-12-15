94a3ec13c57d2c2aa8d8ac79403568ff08f15448
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-15 13:26:37 -0800

    Create a U3Mac project.

---
+++ Compiler/U3Mac/U3Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace U3
+{
+    public class U3Service : Wax.WaxService
+    {
+        public U3Service() : base("u3") { }
+
+        public override Task<Dictionary<string, object>> HandleRequest(Dictionary<string, object> request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
