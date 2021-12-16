55a71cc2f4bb7b8a2ef7579246f266bea4dfe367
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-15 17:08:53 -0800

    First stab at U3 Mac

---
+++ Compiler/U3Mac/U3Window.cs
@@ -0,0 +1,29 @@
+﻿namespace U3
+{
+    public class U3Window
+    {
+        private static int idAlloc = 1;
+        internal int ID { get; set; }
+
+        public U3Window() {
+          this.ID = idAlloc++;
+        }
+
+        public void Show(string title, int width, int height) {
+
+            string html = string.Join('\n', new string[] {
+                "<!DOCTYPE html>",
+                "<html>", "<body>",
+                "U3 code goes here",
+                "</body>",
+                "</html>",
+            });
+            byte[] htmlUtf8 = System.Text.Encoding.UTF8.GetBytes(html);
+            string dataUri = "data:text/html;base64," + System.Convert.ToBase64String(htmlUtf8);
+            show_window(title, width, height, dataUri);
+        }
+
+        [System.Runtime.InteropServices.DllImport("u3native.dylib")]
+        internal static extern show_window(string title, int width, int height);
+    }
+}
---
+++ Compiler/U3Mac/u3native.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "webview.h"
+
+extern "C" {
+#define MODULE_API
+
+  void show_window(const char* title, int width, int height, const char* dataUri);
+}
+
+void show_window(const char* title, int width, int height, const char* dataUri) {
+	webview_t w = webview_create(0, NULL);
+	webview_set_title(w, title);
+	webview_set_size(w, width, height, WEBVIEW_HINT_NONE);
+	webview_navigate(w, dataUri);
+	webview_run(w);
+	webview_destroy(w);
+}
