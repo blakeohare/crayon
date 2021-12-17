43a2b609e4a4a554fb45b5f4a0725227d18cdad4
Blake O'Hare <mspaintmaestro@gmail.com>
2021-12-16 17:03:06 -0800

    Add #if's for MAC in U3

--- Compiler/U3/PlatformUtil.cs
+++ Compiler/U3/PlatformUtil.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace U3
+﻿namespace U3
 {
     internal class PlatformUtil
     {
-        public static readonly string PLATFORM = "win";
+#if WINDOWS
+        internal static readonly string PLATFORM = "win";
+#elif MAC
+        internal static readonly string PLATFORM = "mac";
+#elif LINUX
+        internal static readonly string PLATFORM = "linux";
+#endif
+
+        internal static U3Window CreateWindow(int id)
+        {
+#if WINDOWS
+            return new U3WindowWindows() { ID = id };
+#elif MAC
+            return new U3WindowMac() { ID = id };
+#elif LINUX
+            throw new System.NotImplementedException();
+#else
+            throw new System.NotImplementedException();
+#endif
+        }
     }
 }
