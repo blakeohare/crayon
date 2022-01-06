e7ed828d91c3b437f7537a171f68dbb3c1a15c81
Blake O'Hare <mspaintmaestro@gmail.com>
2022-01-05 18:40:10 -0800

    JavaScript-based runtime service now gets as far as starting.

--- Extensions/ExportWebExtension/source/templates/RouterService.js
+++ Extensions/ExportWebExtension/source/templates/RouterService.js
@@ -1,18 +1,38 @@
-const createRouterServicce = () => {
+const createRouterServicce = (hub) => {
+    let waxhub = hub;
 
-    let handleReqImpl = req => {
+    let handleReqImpl = async req => {
         // TODO: build phase
 
         // Get bundle phase
         console.log(req);
-        throw new Error("Not implemented");
+        let cbxFile = null;
+        if (req.cbxFile) {
+            cbxFile = req.cbxFile;
+        }
+
+        console.log(cbxFile);
 
         // Extension phase
+        // TODO: this
 
         // Run phase
+        if (cbxFile !== null) {
+            let runtimeReq = {
+                realTimePrint: true,
+                args: [],
+                showLibStack: true,
+                useOutputPrefixes: false,
+                cbxPath: cbxFile,
+            };
+
+            let response = await waxhub.sendRequest('runtime', runtimeReq);
+            console.log("RUNTIME COMPLETED:");
+            console.log(response);
+        }
     };
 
-    let handleRequest = async req => {
+    let handleRequest = async (req) => {
         try {
             return handleReqImpl(req);
         } catch (e) {
--- Extensions/ExportWebExtension/source/templates/main.js
+++ Extensions/ExportWebExtension/source/templates/main.js
@@ -1,29 +1,30 @@
-const crayonToolchainRun = (() => {
+const Wax = (() => {
 
     // @INCLUDE@: argParser.js
     // @INCLUDE@: RouterService.js
-    // @INCLUDE@: common.js
     // @INCLUDE@: WaxHub.js
 
     let hub = createWaxHub();
-    hub.registerService(createRouterServicce());
-    hub.registerService(createRuntimeService());
+    hub.registerService(createRouterServicce(hub));
+    hub.registerService(createRuntimeService(hub));
 
     let useU3 = false;
     // @INCLUDE@: USE-U3-OVERRIDE
-    if (useU3) hub.registerService(createU3Service());
+    if (useU3) hub.registerService(createU3Service(hub));
 
     let useBuilder = false; // TODO: one of these days...
     // @INCLUDE@: USE-BUILD-OVERRIDE
     if (useBuilder) {
-        hub.registerService(createBuilderService());
-        hub.registerService(createAssemblyResolverService());
+        hub.registerService(createBuilderService(hub));
+        hub.registerService(createAssemblyResolverService(hub));
     }
 
-    return args => {
-        let cmd = argParse(args);
-        console.log(cmd);
-        hub.sendRequest('router', cmd)
+    return {
+        toolchainRun: args => {
+            let cmd = argParse(args);
+            console.log(cmd);
+            hub.sendRequest('router', cmd)
+        },
     };
 
 })();
