2a469a7046fc6b9bcbe92b0ebfde4e96bfd33288
Blake O'Hare <mspaintmaestro@gmail.com>
2022-01-05 20:49:23 -0800

    Recently the C# U3 embedded resources were acting as the source of truth of the U3 JS source code instead of the Electron version. Now I'm moving it to a central location (U3/js) instead and updating the syncU3.py script to copy from that.

--- Scripts/syncU3.py
+++ Scripts/syncU3.py
@@ -2,43 +2,26 @@ import os
 import sys
 import shutil
 
+def normalize_path(dir):
+  return os.sep.join(dir.split('/'))
+
 def main():
   files = {}
 
-  web_dest_dir = os.path.join('..', 'Compiler', 'Platforms', 'JavaScriptApp', 'ResourcesU3')
-
-  from_dir = os.path.join('..', 'U3', 'src', 'render')
-
-  for file in [
-    'nori.js',
-    'nori_audio.js',
-    'nori_canvas.js',
-    'nori_context.js',
-    'nori_events.js',
-    'nori_gamepad.js',
-    'nori_gl.js',
-    'nori_layout.js',
-    'nori_util.js'
-  ]:
-    from_file = os.path.join(from_dir, file)
-    files[from_file] = [
-      os.path.join(web_dest_dir, file.replace('.', '_') + '.txt')
-    ]
-
-  msghub_client = os.path.join('..', 'Libraries', 'MessageHub', 'client')
-  files[os.path.join(msghub_client, 'js', 'messagehub.js')] = [
-    os.path.join(web_dest_dir, 'messagehub_js.txt')
+  ops = [
+    { "from_dir": '../U3/js',
+      "to_dir": '../Compiler/U3/u3',
+    },
   ]
 
-  msghub_nodejs_client = os.path.join(msghub_client, 'nodejs', 'messagehubclient')
-  print(msghub_nodejs_client )
-  for file in os.listdir(msghub_nodejs_client):
-    files[os.path.join(msghub_nodejs_client, file)] = [os.path.join('..', 'U3', 'src', 'messagehubclient', file)]
-
-  for src_file in files.keys():
-    for dst_file in files[src_file]:
-      shutil.copy(src_file, dst_file)
-      print(src_file + ' --> ' + dst_file)
+  for op in ops:
+    from_dir = op['from_dir']
+    to_dir = op['to_dir']
+    for file in os.listdir(from_dir):
+      from_file = normalize_path(from_dir + '/' + file)
+      to_file = normalize_path(to_dir + '/' + file)
+      print(from_file + ' --> ' + to_file)
+      shutil.copy(from_file, to_file)
 
   print("Done.")
 
