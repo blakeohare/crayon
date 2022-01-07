5019e193c3d8c4bacd00a9b1e30017cbfd05f4f6
Blake O'Hare <mspaintmaestro@gmail.com>
2022-01-06 18:41:48 -0800

    More implementation of the JavaScript toolchain and JS U3.

--- Extensions/ExportWebExtension/source/templates/main.js
+++ Extensions/ExportWebExtension/source/templates/main.js
@@ -1,26 +1,32 @@
 const Wax = (() => {
 
+    let isInitialized = false;
+    let hub = null;
+    
     // @INCLUDE@: argParser.js
     // @INCLUDE@: RouterService.js
     // @INCLUDE@: WaxHub.js
 
-    let hub = createWaxHub();
-    hub.registerService(createRouterServicce(hub));
-    hub.registerService(createRuntimeService(hub));
-
-    let useU3 = false;
-    // @INCLUDE@: USE-U3-OVERRIDE
-    if (useU3) hub.registerService(createU3Service(hub));
-
-    let useBuilder = false; // TODO: one of these days...
-    // @INCLUDE@: USE-BUILD-OVERRIDE
-    if (useBuilder) {
-        hub.registerService(createBuilderService(hub));
-        hub.registerService(createAssemblyResolverService(hub));
-    }
-
     return {
         toolchainRun: args => {
+            if (!isInitialized) {
+                isInitialized = true;
+
+                hub = createWaxHub();
+                hub.registerService(createRouterServicce(hub));
+                hub.registerService(createRuntimeService(hub));
+
+                let useU3 = false;
+                // @INCLUDE@: USE-U3-OVERRIDE
+                if (useU3) hub.registerService(createU3Service(hub));
+
+                let useBuilder = false; // TODO: one of these days...
+                // @INCLUDE@: USE-BUILD-OVERRIDE
+                if (useBuilder) {
+                    hub.registerService(createBuilderService(hub));
+                    hub.registerService(createAssemblyResolverService(hub));
+                }
+            }
             let cmd = argParse(args);
             console.log(cmd);
             hub.sendRequest('router', cmd)
