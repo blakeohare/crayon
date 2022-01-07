02ae19f6066f76eb4e88de9060b734b56bf60a84
Blake O'Hare <mspaintmaestro@gmail.com>
2022-01-06 18:41:48 -0800

    More implementation of the JavaScript toolchain and JS U3.

--- Extensions/ExportWebExtension/source/templates/u3/u3service.js
+++ Extensions/ExportWebExtension/source/templates/u3/u3service.js
@@ -5,21 +5,34 @@
 
 const createU3Service = (hub) => {
     let waxhub = hub;
+    let windowIdAlloc = 1;
     
-    let handleReqImpl = async req => {
-        console.log(req);
-        throw new Error("Not implemented");
-        // TODO: implement window.sendMessage and registerMessageListener here upon creating a U3 instance.
-
-        /*
-       
+    let createU3Instance = () => {
+        // TODO: these need to be encased in a context object that is passed around.
         let listener = null;
+        let windowId = windowIdAlloc++;
         window.registerMessageListener = (cb) => { listener = cb; };
-        
         window.sendMessage = async (msgType, msgPayload) => {
-
+            console.log(msgType, msgPayload);
+            throw new Error("Send message not implemetned yet.");
         };
-        //*/
+        
+        //let uiRoot = document.getElementById('html_render_host');
+        let uiRoot = document.getElementById('crayon_host');
+        actualU3Initialize(uiRoot);
+        return windowId;
+    };
+
+    let handleReqImpl = async req => {
+        switch (req.type) {
+            case 'prepareWindow':
+                let winId = createU3Instance();
+                return { windowId: winId };
+
+            default:
+                console.log(req);
+                throw new Error("Not implemented message type: " + req.type);
+        }
     };
     
     let handleRequest = async (req) => {
