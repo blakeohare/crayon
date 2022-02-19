a62d5f8b29af0061685d648424c948873b928d3a
Blake O'Hare <mspaintmaestro@gmail.com>
2022-02-18 19:06:20 -0800

    Fix console.log in iOS projects so that it appears in STDOUT

--- Extensions/ExportIosExtension/source/templates/project/jsres/ios.js
+++ Extensions/ExportIosExtension/source/templates/project/jsres/ios.js
@@ -9,7 +9,7 @@
   };
 
   window.sendStringToJavaScript = msg => {
-    let parts = msg.split(':', 2);
+    let parts = msg.split(':', 2); // comes in as base64 so 2 will not truncate any parts.
     let msgType = b64Decode(parts[0]);
     let msgValue = b64Decode(parts[1] || "");
     handleMessage(msgType, msgValue);
@@ -19,10 +19,31 @@
     sendStringToSwift("READY", "");
   });
 
+  // TODO: fix this up so that it's closer to parity with Node's STDOUT
+  let stringify = (value) => {
+    if (value === null) return 'null';
+    if (value === undefined) return 'undefined';
+    if (value === true) return 'true';
+    if (value === false) return 'false';
+    if (value === "") return '""';
+    let type = typeof value;
+    if (type === 'number' && isNaN(value)) return 'NaN';
+    if (type === 'object') {
+      try {
+        return JSON.stringify(value);
+      } catch (err) {
+        return Array.isArray(value)
+          ? ("<Array:len=" + value.length + ">")
+          : ("<Object:keys=" + Object.keys(value).join(',') + ">");
+      }
+    }
+    return value + '';
+  };
+
   console.log = function() {
     let sb = [];
     for (let i = 0; i < arguments.length; i++) {
-        sb.push(arguments[i] + '');
+      sb.push(stringify(arguments[i]));
     }
     sendStringToSwift("STDOUT", sb.join(" "));
   };
